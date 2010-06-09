cc72bc8e3b854cc0a8f440402ace8d5901d1a617
Fabian Jakobs <fabian.jakobs@web.de>
2010-06-09 18:59:30 +0200

    implement missing user API methods

--- test/TestUserApi.js
+++ test/TestUserApi.js
@@ -37,14 +37,14 @@ suite.addTests({
     },
 
     "test: get following users" : function(assert, finished) {
-        this.userApi.getFollowing('ornicar', function(err, following) {
+        this.userApi.getFollowing('fjakobs', function(err, following) {
             assert.ok(following.length > 0);
             finished();
         });
     },
 
     "test: get follower users" : function(assert, finished) {
-        this.userApi.getFollowers('ornicar', function(err, followers) {
+        this.userApi.getFollowers('fjakobs', function(err, followers) {
             assert.ok(followers.length > 0);
             finished();
         });
@@ -68,9 +68,74 @@ suite.addTests({
                 finished();
             });
         });
+    },
+
+    "test: follow and unfollow fjakobs" : function(assert, finished, test) {
+        test.github.authenticate(username, token);
+        test.userApi.follow("fjakobs", function(err) {
+            test.userApi.getFollowing(username, function(err, following) {
+                assert.ok(following.indexOf("fjakobs") !== -1);
+
+                test.userApi.unFollow("fjakobs", function(err) {
+                    test.userApi.getFollowing(username, function(err, following) {
+                        assert.ok(following.indexOf("fjakobs") === -1);
+                        finished();
+                    });
+                });
+            });
+        });
+    },
+
+    "test: get watched repos" : function(assert, finished, test) {
+        test.userApi.getWatchedRepos("fjakobs", function(err, repos) {
+            assert.ok(repos.length > 0);
+            assert.ok(repos[0].homepage !== undefined);
+            finished();
+        });
+    },
+
+    "test: get emails" : function(assert, finished, test) {
+        test.github.authenticate(username, token);
+        test.userApi.getEmails(function(err, mails) {
+            assert.ok(mails.indexOf("fabian@ajax.org") !== -1);
+            finished();
+        });
+    },
+
+    "test: add and remove email" : function(assert, finished, test) {
+        test.github.authenticate(username, token);
+        test.userApi.addEmail("juhu@ajax.org", function(err, mails) {
+            assert.ok(mails.indexOf("juhu@ajax.org") !== -1);
+
+            test.userApi.removeEmail("juhu@ajax.org", function(err, mails) {
+                assert.ok(mails.indexOf("juhu@ajax.org") === -1);
+                finished();
+            });
+        });
+    },
+
+    "test: get ssh keys" : function(assert, finished, test) {
+        test.github.authenticate(username, token);
+        test.userApi.getKeys(function(err, keys) {
+            assert.ok(keys[0].title !== undefined);
+            finished();
+        });
+    },
+
+    "test: add and remove ssh keys" : function(assert, finished, test) {
+        var sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq3iNw6EbjbV7M+Y74bMaRJ2V4fhGbpCPf4r5LziRlbkwZ2odG3Zkxzvhgg6EGxh6M4PWwiTF/gK/2nk81aAGN8iKvqS8b8TML/0RHrYyvR2Okug+CR5LbLVO+yM23nAixadhrZqTreqqgjJvqF4ffD0rsfVyqaYAsNxDoqvLFaEyMqh0+gNsO20M1jRLjQqeA4gzvQhjMWSnzOzBpCorCECjhl9o7iqitDaTzUWPLB4V0jnuyG15nbDOrCmzA8l9wIrqSjI6Kglx2aZWRKcsEaCUPHD5n4F63og+8aHRPMaEvNCvKj/21a0/zhEtdh1Vd3etmfe38Rh4WmOyuv5L+Q== test@ajax.org";
+
+        test.github.authenticate(username, token);
+        test.userApi.addKey("test2", sshKey, function(err, keys) {
+            var key = keys.filter(function(key) { return key.title == "test2"; })[0];
+            assert.equal(key.key, sshKey.replace(" test@ajax.org", ""));
+            test.userApi.removeKey(key.id, function(err, keys) {
+                var key = keys.filter(function(key) { return key.title == "test2"; })[0];
+                assert.equal(key, undefined);
+                finished();
+            });
+        });
     }
-
-
 });
 
 if (module === require.main) {
