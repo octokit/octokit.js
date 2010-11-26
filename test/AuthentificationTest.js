fd9a12baf2ec4e88ec8a4b13ccaa4d9e81a6b327
Fabian Jakobs <fabian.jakobs@web.de>
2010-11-26 14:10:10 +0100

    add support for authentication using: 	- an OAuth access token 	- username/password 	- username/API token

--- test/AuthentificationTest.js
+++ test/AuthentificationTest.js
@@ -12,8 +12,9 @@ var async_testing = require('../vendor/node-async-testing/async_testing');
 
 var suite = exports.suite = new async_testing.TestSuite();
 
-var username = 'fjakobstest';
-var token = 'b98166e45acf66df70a992e2de56b92a';
+var username = "fjakobstest";
+var token = "b98166e45acf66df70a992e2de56b92a";
+
 var repo = "o3";
 
 suite.setup(function() {
@@ -31,15 +32,32 @@ suite.addTests({
     },
 
     "test: show user with authentification should have a 'plan'" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.show(username, function(err, user) {
             assert.ok(user.plan !== undefined);
             finished();
         });
     },
 
+    // test disabled because I don't want to see my password on github :-)
+//    "test: authenticate using username and password" : function(assert, finished, test) {
+//	    test.github.authenticatePassword(username, "XXXX");
+//	    test.userApi.show(username, function(err, user) {
+//	        assert.ok(user.plan !== undefined);
+//	        finished();
+//	    });
+//	},
+	
+    "test: authenticate using username and wrong password" : function(assert, finished, test) {
+	    test.github.authenticatePassword(username, "1234");
+	    test.userApi.show(username, function(err, user) {
+	        assert.ok(err !== undefined);
+	        finished();
+	    });
+	},
+	
     "test: authenticate with bad token" : function(assert, finished, test) {
-        test.github.authenticate(username, "bad-token");
+        test.github.authenticateToken(username, "bad-token");
         test.userApi.show(username, function(err, user) {
             assert.ok(err !== undefined);
             finished();
--- test/UserApiTest.js
+++ test/UserApiTest.js
@@ -24,6 +24,7 @@ suite.setup(function() {
 suite.addTests({
    "test: search users" : function(assert, finished) {
         this.userApi.search(username, function(err, users) {
+            assert.equal(err, null);
             assert.equal(users.length, 1);
             assert.equal(users[0].name, username);
             finished();
@@ -32,6 +33,7 @@ suite.addTests({
 
     "test: show a user" : function(assert, finished) {
         this.userApi.show(username, function(err, user) {
+            assert.equal(err, null);
             assert.equal(user.login, username);
             finished();
         });
@@ -47,6 +49,7 @@ suite.addTests({
 
     "test: get following users" : function(assert, finished) {
         this.userApi.getFollowing('fjakobs', function(err, following) {
+            assert.equal(err, null);
             assert.ok(following.length > 0);
             finished();
         });
@@ -54,14 +57,16 @@ suite.addTests({
 
     "test: get follower users" : function(assert, finished) {
         this.userApi.getFollowers('fjakobs', function(err, followers) {
+            assert.equal(err, null);
             assert.ok(followers.length > 0);
             finished();
         });
     },
 
     "test: authenticate user and update location to Argentinia" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.update(username, {location: "Argentinia"}, function(err) {
+            assert.equal(err, null);
             test.userApi.show(username, function(err, user) {
                 assert.equal(user.location, "Argentinia");
                 finished();
@@ -70,7 +75,7 @@ suite.addTests({
     },
 
     "test: authenticate user and update location to France" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.update(username, {location: "France"}, function(err) {
             test.userApi.show(username, function(err, user) {
                 assert.equal(user.location, "France");
@@ -80,7 +85,7 @@ suite.addTests({
     },
 
     "test: follow and unfollow fjakobs" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.follow("fjakobs", function(err) {
             test.userApi.getFollowing(username, function(err, following) {
                 assert.ok(following.indexOf("fjakobs") !== -1);
@@ -97,6 +102,7 @@ suite.addTests({
 
     "test: get watched repos" : function(assert, finished, test) {
         test.userApi.getWatchedRepos("fjakobs", function(err, repos) {
+            assert.equal(err, null);
             assert.ok(repos.length > 0);
             assert.ok(repos[0].homepage !== undefined);
             finished();
@@ -104,16 +110,18 @@ suite.addTests({
     },
 
     "test: get emails" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.getEmails(function(err, mails) {
+            assert.equal(err, null);
             assert.ok(mails.indexOf("fabian@ajax.org") !== -1);
             finished();
         });
     },
 
     "test: add and remove email" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.addEmail("juhu@ajax.org", function(err, mails) {
+            assert.equal(err, null);
             assert.ok(mails.indexOf("juhu@ajax.org") !== -1);
 
             test.userApi.removeEmail("juhu@ajax.org", function(err, mails) {
@@ -124,8 +132,9 @@ suite.addTests({
     },
 
     "test: get ssh keys" : function(assert, finished, test) {
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.getKeys(function(err, keys) {
+            assert.equal(err, null);
             assert.ok(keys[0].title !== undefined);
             finished();
         });
@@ -134,8 +143,9 @@ suite.addTests({
     "test: add and remove ssh keys" : function(assert, finished, test) {
         var sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq3iNw6EbjbV7M+Y74bMaRJ2V4fhGbpCPf4r5LziRlbkwZ2odG3Zkxzvhgg6EGxh6M4PWwiTF/gK/2nk81aAGN8iKvqS8b8TML/0RHrYyvR2Okug+CR5LbLVO+yM23nAixadhrZqTreqqgjJvqF4ffD0rsfVyqaYAsNxDoqvLFaEyMqh0+gNsO20M1jRLjQqeA4gzvQhjMWSnzOzBpCorCECjhl9o7iqitDaTzUWPLB4V0jnuyG15nbDOrCmzA8l9wIrqSjI6Kglx2aZWRKcsEaCUPHD5n4F63og+8aHRPMaEvNCvKj/21a0/zhEtdh1Vd3etmfe38Rh4WmOyuv5L+Q== test@ajax.org";
 
-        test.github.authenticate(username, token);
+        test.github.authenticateToken(username, token);
         test.userApi.addKey("test2", sshKey, function(err, keys) {
+            assert.equal(err, null);
             var key = keys.filter(function(key) { return key.title == "test2"; })[0];
             assert.equal(key.key, sshKey.replace(" test@ajax.org", ""));
             test.userApi.removeKey(key.id, function(err, keys) {
