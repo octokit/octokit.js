534ed08caa0baa236073a46021ce854340449826
Tim Walling <tim.walling@gmail.com>
2012-08-31 12:39:22 -0400

    add new repo merging API

--- api/v3.0.0/repos.js
+++ api/v3.0.0/repos.js
@@ -324,6 +324,49 @@ var repos = module.exports = {
     };
 
     /** section: github
+     *  repos#merge(msg, callback) -> null
+     *      - msg (Object): Object that contains the parameters and their values to be sent to the server.
+     *      - callback (Function): function to call when the request is finished with an error as first argument and result data as second argument.
+     * 
+     *  ##### Params on the `msg` object:
+     * 
+     *  - user (String): Required. 
+     *  - repo (String): Required. 
+     *  - base (String): Required. The name of the base branch that the head will be merged into. 
+     *  - head (String): Required. The head to merge. This can be a branch name or a commit SHA1.
+     *  - commit_message (String): Optional. Commit message to use for the merge commit. If omitted, a default message will be used.
+     **/
+    this.merge = function(msg, block, callback) {
+        var self = this;
+        this.client.httpSend(msg, block, function(err, res) {
+            if (err)
+                return self.sendError(err, null, msg, callback);
+
+            var ret;
+            try {
+                ret = res.data && JSON.parse(res.data);
+            }
+            catch (ex) {
+                if (callback)
+                    callback(new error.InternalServerError(ex.message), res);
+                return;
+            }
+            
+            if (!ret)
+                ret = {};
+            if (!ret.meta)
+                ret.meta = {};
+            ["x-ratelimit-limit", "x-ratelimit-remaining", "link"].forEach(function(header) {
+                if (res.headers[header])
+                    ret.meta[header] = res.headers[header];
+            });
+            
+            if (callback)
+                callback(null, ret);
+        });
+    }
+
+    /** section: github
      *  repos#getContributors(msg, callback) -> null
      *      - msg (Object): Object that contains the parameters and their values to be sent to the server.
      *      - callback (Function): function to call when the request is finished with an error as first argument and result data as second argument.
