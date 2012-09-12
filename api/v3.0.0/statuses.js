b73697369a7bccc2c1b84455f02ed816ad73ebb0
mikedeboer <info@mikedeboer.nl>
2012-09-12 22:10:11 +0200

    fix for docs

--- api/v3.0.0/statuses.js
+++ api/v3.0.0/statuses.js
@@ -1,10 +1,21 @@
+/**
+ *  mixin statuses
+ *
+ *  Copyright 2012 Cloud9 IDE, Inc.
+ *
+ *  This product includes software developed by
+ *  Cloud9 IDE, Inc (http://c9.io).
+ *
+ *  Author: Mike de Boer <mike@c9.io>
+ **/
+
 "use strict";
 
 var error = require("./../../error");
 var Util = require("./../../util");
 
 var statuses = module.exports = {
-	statuses: {}
+    statuses: {}
 };
 
 (function() {
