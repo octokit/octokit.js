cdff93db89979b562e551047f24b3fcce41b33e0
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2014-10-29 09:34:03 -0700

    Clean up markdown render method

--- api/v3.0.0/markdown.js
+++ api/v3.0.0/markdown.js
@@ -37,17 +37,10 @@ var markdown = module.exports = {
             if (err)
                 return self.sendError(err, null, msg, callback);
 
-            var ret = {};
-            ret.meta = ret.meta || {};
-
-            try {
-                ret.body = res.data && JSON.parse(JSON.stringify(res.data));
-            }
-            catch (ex) {
-                if (callback)
-                    callback(new error.InternalServerError(ex.message), res);
-                return;
-            }
+            var ret = {
+                body: res.data,
+                meta: {}
+            };
 
             ["x-ratelimit-limit", "x-ratelimit-remaining", "x-ratelimit-reset", "x-oauth-scopes", "link", "location", "last-modified", "etag", "status"].forEach(function(header) {
                 if (res.headers[header]) {
