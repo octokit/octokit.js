25fa7f6d994d77ad961be6bfab7c0b127d71bf7f
Joe Gallo <joe@brassafrax.com>
2016-01-25 23:54:20 -0500

    add search most starred repos example

---
+++ examples/search_most_starred_repos.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var Client = require("./../index");
+var testAuth = require("./../test_auth.json");
+
+var github = new Client({
+    // debug: true
+});
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
+});
+
+github.search.repos({
+    q: "stars:>=20000",
+    sort: "stars",
+    order: "desc"
+}, function(err, res) {
+    for (var itemKey in res['items']) {
+        var item = res['items'][itemKey];
+        var url = item['html_url'];
+        var star_count = item['stargazers_count'];
+        console.log(url + " (" + star_count + ")");
+    }
+});
