7dd35dc581812c05edeee4db4b27ca643a34ddd5
Joe Gallo <joe@brassafrax.com>
2016-01-25 20:53:04 -0500

    add get following example

---
+++ examples/get_following.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Client = require("./../index");
+var testAuth = require("./../test_auth.json");
+
+var github = new Client({
+    debug: true
+});
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
+});
+
+github.users.getFollowingForUser({
+    user: "defunkt"
+}, function(err, res) {
+    console.log(err, res);
+});
