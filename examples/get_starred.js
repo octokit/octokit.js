4479e206c6b6a2acc91d521fa36f837792658714
Joe Gallo <joe@brassafrax.com>
2016-02-13 21:44:54 -0500

    have get_starred example get all results
    Have at least one example showing how to get all results using
    getNextPage.

--- examples/get_starred.js
+++ examples/get_starred.js
@@ -3,16 +3,30 @@
 var Client = require("./../lib/index");
 var testAuth = require("./../test_auth.json");
 
-var github = new Client({
-    debug: true
-});
+var github = new Client({});
 
 github.authenticate({
     type: "oauth",
     token: testAuth["token"]
 });
 
-github.activity.getStarredRepos({
-}, function(err, res) {
-    console.log(err, res);
-});
+var starredRepos = [];
+
+var req = github.activity.getStarredRepos({ per_page: 100}, getRepos);
+function getRepos(err, res) {
+    if (err) {
+        return false;
+    }
+
+    starredRepos = starredRepos.concat(res);
+    if (github.hasNextPage(res)) {
+        github.getNextPage(res, getRepos)
+    } else {
+        outputStarredRepos();
+    }
+}
+
+function outputStarredRepos() {
+    console.log('starred repos: ' + starredRepos.length);
+    console.log(starredRepos.map(function(repo) { return repo['full_name']; }));
+}
