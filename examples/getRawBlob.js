c4ab8fe11ac64bf2e9c0532a51df6d10c54bbe79
Joe Gallo <joe@brassafrax.com>
2016-06-26 20:46:31 -0400

    add get raw blob example

---
+++ examples/getRawBlob.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var Client = require("./../lib/index");
+var testAuth = require("./../testAuth.json");
+
+var github = new Client({
+    debug: false,
+    headers: {
+        "Accept": "application/vnd.github.v3.raw"
+    }
+});
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
+});
+
+// github.repos.getContent({
+//     user: "mikedeboer",
+//     repo: "node-github",
+//     path: "README.md"
+// }, function(err, res) {
+//     var sha = res["sha"];
+//     console.log(sha);
+// });
+
+github.gitdata.getBlob({
+    user: "mikedeboer",
+    repo: "node-github",
+    sha: "b361f529df9b49f2a6b5748b5d71b792c8383e5e"
+}, function(err, res) {
+    console.log(res['data']);
+});
