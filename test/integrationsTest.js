cf3f6a6ece607e7a1080a0ba15b6f1ad98b89b35
Ramon Snir <r@mon.co.il>
2016-10-04 12:51:08 -0400

    add integrations/get-installations to routes.json

---
+++ test/integrationsTest.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2012 Cloud9 IDE, Inc.
+ *
+ * This product includes software developed by
+ * Cloud9 IDE, Inc (http://c9.io).
+ *
+ * Author: Mike de Boer <info@mikedeboer.nl>
+ */
+
+"use strict";
+
+var Assert = require("assert");
+var Client = require("./../lib/index");
+var testAuth = require("./../testAuth.json");
+
+describe("[integrations]", function() {
+    var client;
+    var token = testAuth["token"];
+
+    beforeEach(function() {
+        client = new Client();
+        client.authenticate({
+            type: "oauth",
+            token: token
+        });
+    });
+
+    it("should successfully execute GET /integration/installations (getInstallations)",  function(next) {
+        client.integrations.getInstallations(
+            {
+                page: "Number",
+                per_page: "Number"
+            },
+            function(err, res) {
+                Assert.equal(err, null);
+                // other assertions go here
+                next();
+            }
+        );
+    });
+});
