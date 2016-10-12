0c638464ad3ad96f3a0cbf7cfd9bfae615cdf1d0
Joe Gallo <joe@brassafrax.com>
2016-10-12 15:35:08 -0400

    add createStatus example

---
+++ examples/createStatus.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Client = require("./../lib/index");
+var testAuth = require("./../testAuth.json");
+
+var github = new Client({
+    debug: true
+});
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
+});
+
+github.repos.createStatus({
+    owner: "kaizensoze",
+    repo: "test2",
+    sha: "81c559e2e8551982235bc86594cd86ffb135b053",
+    state: "success",
+}, function(err, res) {
+    console.log(err, res);
+});
