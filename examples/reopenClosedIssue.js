e1532b2617cbaaf8738d9ae49b9ad9ae5aab9412
Joe Gallo <joe@brassafrax.com>
2017-09-27 10:10:53 -0400

    add reopen closed issue example

---
+++ examples/reopenClosedIssue.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Client = require("./../lib/index");
+var testAuth = require("./../testAuth.json");
+
+var github = new Client({
+    debug: true
+});
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
+});
+
+github.issues.edit({
+    owner: "kaizensoze",
+    repo: "test2",
+    number: 2,
+    state: "open"
+}, function(err, res) {
+    console.log(err, res);
+});
