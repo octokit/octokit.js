5ccc136e083fdbce9d270d8e31928ecdb96f14b5
Gregor <gregor@martynus.net>
2017-11-27 10:16:13 -0800

    chore: lowercase scripts file names

---
+++ scripts/generate-typescript-types.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+module.exports = require('./generate-types')('TypeScript', 'index.d.ts.tpl', 'index.d.ts')
--- scripts/generateFlowTypes.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-module.exports = require('./generateTypes')('Flow', 'index.js.flow.tpl', 'index.js.flow')
