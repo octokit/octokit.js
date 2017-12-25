19ece73d4555d6c00b714889eeaf56514e462f8b
Travis CI User <travis@example.org>
2017-12-25 23:30:12 +0000

    Updates

--- api_project.js
+++ api_project.js
@@ -4,13 +4,13 @@ define({
   },
   "name": "github",
   "version": "0.0.0-development",
-  "description": "NodeJS wrapper for the GitHub API",
+  "description": "GitHub REST API client for Node.js",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-02T00:11:56.877Z",
+    "time": "2017-12-25T23:29:47.271Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
