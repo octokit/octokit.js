f1ca2b75846e804d0dc1d1663aab700cde5761b4
Gregor <gregor@martynus.net>
2017-12-28 13:53:40 -0800

    wip

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = GitHubApi
 
 const authenticate = require('./lib/authenticate')
-const parseOptions = require('./lib/parse-options')
+const parseClientOptions = require('./lib/parse-client-options')
 const setupApiMethods = require('./lib/setup')
 const paginationApi = require('./lib/pagination')
 
 const PREVIEW_HEADER_BY_PATH = require('./lib/definitions/preview-header-by-path.json')
 
 function GitHubApi (options) {
-  const state = parseOptions(options)
+  const state = parseClientOptions(options)
   state.previewHeaderByPath = PREVIEW_HEADER_BY_PATH
   const routesApi = setupApiMethods(state)
 
