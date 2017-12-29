14f6e2303dc582461470f70bf7b6378d35054e34
Gregor <gregor@martynus.net>
2017-12-28 16:44:51 -0800

    wip

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const parseClientOptions = require('./lib/parse-client-options')
 const setupApiMethods = require('./lib/setup')
 const paginationApi = require('./lib/pagination')
 
-const PREVIEW_HEADER_BY_PATH = require('./lib/definitions/preview-header-by-path.json')
-
 function GitHubApi (options) {
   const state = parseClientOptions(options)
-  state.previewHeaderByPath = PREVIEW_HEADER_BY_PATH
   const routesApi = setupApiMethods(state)
 
   return Object.assign(routesApi, paginationApi, {
