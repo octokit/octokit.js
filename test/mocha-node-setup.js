ac59ddc304b46dad824e2dee0c261d49cfa6b755
Gregor <gregor@martynus.net>
2018-02-24 19:34:57 -0800

    test: mock Cypress only in node

--- test/mocha-node-setup.js
+++ test/mocha-node-setup.js
@@ -1,25 +1,27 @@
 // non-Cypresss environment (plain Mocha)
 // simulate Cypress built-in tools, like sinon-chai
 
-const chai = require('chai')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-chai.use(sinonChai)
-global.expect = chai.expect
+if (!global.cy) {
+  const chai = require('chai')
+  const sinon = require('sinon')
+  const sinonChai = require('sinon-chai')
+  chai.use(sinonChai)
+  global.expect = chai.expect
 
-let sandbox
-beforeEach(() => {
-  sandbox = sinon.createSandbox()
-  global.cy = {
-    stub: function () {
-      return sandbox.stub.apply(sandbox, arguments)
-    },
-    log () {
-      console.log.apply(console, arguments)
+  let sandbox
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+    global.cy = {
+      stub: function () {
+        return sandbox.stub.apply(sandbox, arguments)
+      },
+      log () {
+        console.log.apply(console, arguments)
+      }
     }
-  }
-})
+  })
 
-afterEach(() => {
-  sandbox.restore()
-})
+  afterEach(() => {
+    sandbox.restore()
+  })
+}
