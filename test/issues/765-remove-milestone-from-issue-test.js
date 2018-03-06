af3b404dbfd3293adab136156abd93c4e4e4ad12
Gregor <gregor@martynus.net>
2018-03-06 11:25:17 -0800

    test: remove milestone from issue

---
+++ test/issues/765-remove-milestone-from-issue-test.js
@@ -0,0 +1,23 @@
+const nock = require('nock')
+const Octokit = require('../../')
+
+require('../mocha-node-setup')
+
+describe('https://github.com/octokit/rest.js/issues/765', () => {
+  it('octokit.issues.edit({..., milestone: null})', () => {
+    nock('https://api.github.com')
+      .patch('/repos/epmatsw/example-repo/issues/1', (body) => {
+        expect(body).to.deep.equal({milestone: null})
+        return true
+      })
+      .reply(200, {})
+
+    const client = new Octokit()
+    return client.issues.edit({
+      owner: 'epmatsw',
+      repo: 'example-repo',
+      milestone: null,
+      number: 1
+    })
+  })
+})
