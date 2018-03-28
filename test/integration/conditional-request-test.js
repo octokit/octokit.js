77b05b04062d3012cef493fd298fa55b27d71d75
Gregor <gregor@martynus.net>
2018-03-28 15:51:57 -0700

    test: throw on 304

--- test/integration/conditional-request-test.js
+++ test/integration/conditional-request-test.js
@@ -19,18 +19,23 @@ describe('request 304s', () => {
       .reply(304, '')
 
     return github.orgs.get({org: 'myorg', headers: {'If-None-Match': 'etag'}})
-
+      .then((response) => {
+        expect.fail('should throw error')
+      })
       .catch(exception => {
         expect(exception.code).to.equal(304)
       })
   })
+
   it('304 last-modified', () => {
     nock('https://request-errors-test.com')
       .get('/orgs/myorg')
       .reply(304, '')
 
     return github.orgs.get({org: 'myorg', headers: {'If-Modified-Since': 'Sun Dec 24 2017 22:00:00 GMT-0600 (CST)'}})
-
+      .then((response) => {
+        expect.fail('should throw error')
+      })
       .catch(exception => {
         expect(exception.code).to.equal(304)
       })
