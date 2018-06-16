d4f9a8010548762a5c877030dd425e45ff5951a8
Gregor <gregor@martynus.net>
2018-06-16 13:34:30 -0700

    test: create check with empty actions array (#922)

---
+++ test/issues/922-create-check-with-empty-actions-array-test.js
@@ -0,0 +1,29 @@
+const nock = require('nock')
+const Octokit = require('../../')
+
+require('../mocha-node-setup')
+
+describe('https://github.com/octokit/rest.js/issues/922', () => {
+  it('octokit.issues.edit({..., milestone: null})', () => {
+    nock('https://api.github.com')
+      .post('/repos/chrisvariety/test/check-runs')
+      .reply(200, [])
+
+    const client = new Octokit()
+    return client.checks.create({
+      owner: 'chrisvariety',
+      repo: 'test',
+      name: 'QA',
+      head_sha: 'SHA',
+      head_branch: 'XXX',
+      status: 'in_progress',
+      started_at: '2018-01-01T06:00:00Z',
+      output: {
+        title: 'Test',
+        summary: 'Test!',
+        text: 'Test'
+      },
+      actions: []
+    })
+  })
+})
