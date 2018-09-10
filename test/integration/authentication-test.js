caa2375d24bf75c332d6fef40bbd59beb354c7ea
Ben Scott <ben.scott@shopify.com>
2018-09-10 15:18:52 -0700

    Add tests

--- test/integration/authentication-test.js
+++ test/integration/authentication-test.js
@@ -106,7 +106,7 @@ describe('authentication', () => {
     return github.repos.getForOrg({org: 'myorg', per_page: 1})
   })
 
-  it('app', () => {
+  it('app with token', () => {
     nock('https://authentication-test-host.com', {
       reqheaders: {
         authorization: 'Bearer abc4567'
@@ -121,6 +121,132 @@ describe('authentication', () => {
     })
 
     return github.orgs.get({org: 'myorg'})
+  })
+
+  it('app with appTokenGenerator', () => {
+    const tokenGenerator = () => 'abcdef'
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.machine-man-preview+json',
+        authorization: 'Bearer abcdef'
+      }
+    })
+      .get('/app/installations')
+      .reply(200, {})
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'Bearer v.xyx789'
+      }
+    })
+      .get('/orgs/myorg')
+      .reply(200, {})
+
+    github.authenticate({
+      type: 'app',
+      appTokenGenerator: tokenGenerator
+    })
+
+    return github.apps.getInstallations({})
+  })
+
+  it('app with appTokenGenerator and installationId - reusing cached token', () => {
+    const tokenGenerator = () => 'abcdef'
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.machine-man-preview+json',
+        authorization: 'Bearer abcdef'
+      }
+    })
+      .post('/app/installations/456/access_tokens')
+      .once()
+      .reply(201, {
+        'token': 'v.xyx789',
+        'expires_at': '2118-09-01T00:00:00Z'
+      })
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'Bearer v.xyx789'
+      }
+    })
+      .get('/orgs/myorg')
+      .twice()
+      .reply(200, {})
+
+    github.authenticate({
+      type: 'app',
+      appTokenGenerator: tokenGenerator,
+      installationId: 456
+    })
+
+    return github.orgs.get({org: 'myorg'}).then(() => {
+      // Make a second request to prove that the token is reused
+      github.orgs.get({org: 'myorg'})
+    })
+  })
+
+  it('app with appTokenGenerator and installationId - requesting new token after expiry', () => {
+    const tokenGenerator = () => {
+      return 'abcdef'
+    }
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.machine-man-preview+json',
+        authorization: 'Bearer abcdef'
+      }
+    })
+      .post('/app/installations/456/access_tokens')
+      .once()
+      .reply(201, {
+        'token': 'v.xyx789',
+        'expires_at': '2008-09-01T00:00:00Z'
+      })
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        accept: 'application/vnd.github.machine-man-preview+json',
+        authorization: 'Bearer abcdef'
+      }
+    })
+      .post('/app/installations/456/access_tokens')
+      .once()
+      .reply(201, {
+        'token': 'v.ghj567',
+        'expires_at': '2008-09-01T00:00:00Z'
+      })
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'Bearer v.xyx789'
+      }
+    })
+      .get('/orgs/myorg')
+      .once()
+      .reply(200, {})
+
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'Bearer v.ghj567'
+      }
+    })
+      .get('/orgs/myorg')
+      .once()
+      .reply(200, {})
+
+    github.authenticate({
+      type: 'app',
+      appTokenGenerator: tokenGenerator,
+      installationId: 456
+    })
+
+    return github.orgs.get({org: 'myorg'}).then(() => {
+      // Make a second request to prove that the token is rerequested
+      github.orgs.get({org: 'myorg'})
+    }).catch(() => {})
   })
 
   it('authenticate without options', () => {
