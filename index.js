b652d473a770ecc10dc6c250c44ab038572c9c83
Gregor <gregor@martynus.net>
2018-10-31 14:44:34 -0700

    feat: octokit.request & octokit.request.endpoint

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 module.exports = GitHubApi
 
-const defaultsDeep = require('lodash/defaultsDeep')
+const endpoint = require('@octokit/request').endpoint
 const Hook = require('before-after-hook')
 
 const parseClientOptions = require('./lib/parse-client-options')
-const request = require('./lib/request')
-const ENDPOINT_DEFAULTS = require('./lib/endpoint').DEFAULTS
+const requestWithDefaults = require('./lib/request-with-defaults')
 
 const PLUGINS = [
   require('./lib/plugins/authentication'),
@@ -14,15 +13,13 @@ const PLUGINS = [
 ]
 
 function GitHubApi (options) {
-  const defaults = defaultsDeep(parseClientOptions(options), ENDPOINT_DEFAULTS)
-
   const hook = new Hook()
   const api = {
-    // NOTE: github.hook, github.plugin and github.request are experimental APIs
+    // NOTE: github.hook and github.plugin are experimental APIs
     //       at this point and can change at any time
     hook,
     plugin: (pluginFunction) => pluginFunction(api),
-    request: (options) => api.hook('request', defaultsDeep(options, defaults), request)
+    request: requestWithDefaults(hook, endpoint, parseClientOptions(options))
   }
 
   PLUGINS.forEach(api.plugin)
