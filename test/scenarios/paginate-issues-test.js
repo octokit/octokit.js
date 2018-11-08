62cda2e6a51321576603f8e50f76a51facdbb058
Gregor <gregor@martynus.net>
2018-11-08 11:20:46 -0800

    test: .paginate & .paginate.iterator

---
+++ test/scenarios/paginate-issues-test.js
@@ -0,0 +1,72 @@
+const { getInstance } = require('../util')
+
+require('../mocha-node-setup')
+
+if (!process.browser && parseInt(process.version.substr(1), 10) >= 10) {
+  require('./paginate-issues-async-await.js')
+}
+
+describe('api.github.com', () => {
+  let github
+
+  beforeEach(() => {
+    return getInstance('paginate-issues')
+
+      .then(instance => {
+        github = instance
+
+        github.authenticate({
+          type: 'token',
+          token: '0000000000000000000000000000000000000001'
+        })
+      })
+  })
+
+  it('paginate using ayncIterator', () => {
+    const options = {
+      owner: 'octokit-fixture-org',
+      repo: 'paginate-issues',
+      per_page: 3,
+      headers: {
+        accept: 'application/vnd.github.v3+json'
+      }
+    }
+
+    const iterator = github.paginate.iterator(github.issues.getForRepo.endpoint.merge(options))[Symbol.asyncIterator]()
+
+    return iterator.next()
+      .then(result => {
+        // page 1, results 1 - 3
+        expect(result.value.data.length).to.equal(3)
+
+        return iterator.next()
+      })
+      .then(result => {
+        // page 2, results 4 - 6
+        expect(result.value.data.length).to.equal(3)
+
+        return iterator.next()
+      })
+      .then(result => {
+        // page 3, results 7 - 9
+        expect(result.value.data.length).to.equal(3)
+
+        return iterator.next()
+      })
+      .then(result => {
+        // page 4, results 10 - 12
+        expect(result.value.data.length).to.equal(3)
+
+        return iterator.next()
+      })
+      .then(result => {
+        // page 5, results 13
+        expect(result.value.data.length).to.equal(1)
+
+        return iterator.next()
+      })
+      .then(result => {
+        expect(result.done).to.equal(true)
+      })
+  })
+})
