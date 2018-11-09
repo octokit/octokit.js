4775458f7b50d76c65f6dd2ad187b1fbb17ac161
Gregor <gregor@martynus.net>
2018-11-08 18:45:47 -0800

    test: autopaginate using parameter

--- test/integration/pagination-test.js
+++ test/integration/pagination-test.js
@@ -44,6 +44,44 @@ describe('pagination', () => {
         })
     ])
   })
+
+  it('autopagination', () => {
+    nock('https://pagination-test.com')
+      .get('/organizations')
+      .query({ page: 1, per_page: 1 })
+      .reply(200, [{ id: 1 }], {
+        'Link': '<https://pagination-test.com/organizations?page=2&per_page=1>; rel="next"',
+        'X-GitHub-Media-Type': 'github.v3; format=json'
+      })
+      .get('/organizations')
+      .query({ page: 2, per_page: 1 })
+      .reply(200, [{ id: 2 }], {
+        'X-GitHub-Media-Type': 'github.v3; format=json'
+      })
+      .persist()
+
+    const github = new GitHub({
+      baseUrl: 'https://pagination-test.com'
+    })
+
+    github.hook.wrap('request', (request, options) => {
+      if (!options.request.paginate) {
+        return request(options)
+      }
+
+      delete options.request.paginate
+      return github.paginate(options)
+    })
+
+    return github.request('GET /organizations', { per_page: 1, request: { paginate: true } })
+      .then(organizations => {
+        expect(organizations).to.deep.equal([
+          { id: 1 },
+          { id: 2 }
+        ])
+      })
+  })
+
   it('.paginate.iterator for end endpoints that don’t paginate', () => {
     nock('https://pagination-test.com')
       .get('/orgs/myorg')
