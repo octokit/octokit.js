38804459fb8f30dafcf36da7509de4e4d30e3309
Gregor <gregor@martynus.net>
2018-11-09 07:14:17 -0800

    test: don’t run async iterators in browsers yet

--- test/scenarios/paginate-issues-async-await.js
+++ test/scenarios/paginate-issues-async-await.js
@@ -4,8 +4,14 @@
 const { getInstance } = require('../util')
 
 describe('api.github.com', () => {
-  // const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)/)[1])
   let github
+
+  if (global.navigator) {
+    const chromeVersion = parseInt(global.navigator.appVersion.match(/Chrome\/(\d+)/).pop(), 10)
+    if (chromeVersion < 63) {
+      return it.skip(`for await (let result of github.paginate.iterator() (Chrome v${chromeVersion} does not support async iterators, they were introduced in v63)`)
+    }
+  }
 
   beforeEach(() => {
     return getInstance('paginate-issues')
@@ -20,7 +26,7 @@ describe('api.github.com', () => {
       })
   })
 
-  it('for await (let result of github.issues.getForRepo.paginate()', async () => {
+  it('for await (let result of github.paginate.iterator()', async () => {
     const options = {
       owner: 'octokit-fixture-org',
       repo: 'paginate-issues',
