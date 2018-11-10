c98bdfc4f5e13ff2416c4be0d5097613ed8fa94e
Gregor <gregor@martynus.net>
2018-11-10 10:08:08 -0800

    build: temporary script to remove all deprecated methods and parameters

---
+++ tmp/remove-deprecated-methods-and-params.js
@@ -0,0 +1,24 @@
+const routes = require('../plugins/rest-api-endpoints/routes')
+
+Object.keys(routes).forEach(scope => {
+  Object.keys(routes[scope]).forEach(methodName => {
+    if (routes[scope][methodName].alias && !routes[scope][methodName].deprecated) {
+      console.log(`${scope}.${methodName} has alias, but not deprecated`)
+    }
+    if (routes[scope][methodName].deprecated) {
+      delete routes[scope][methodName]
+      return
+    }
+
+    Object.keys(routes[scope][methodName].params).forEach(param => {
+      if (routes[scope][methodName].params[param].alias && !routes[scope][methodName].params[param].deprecated) {
+        console.log(`${scope}.${methodName}#${param} has alias, but not deprecated`)
+      }
+      if (routes[scope][methodName].params[param].deprecated) {
+        delete routes[scope][methodName].params[param]
+      }
+    })
+  })
+})
+
+require('fs').writeFileSync(require('path').join(__dirname, '..', 'plugins', 'rest-api-endpoints', 'routes.json'), JSON.stringify(routes, null, 2) + '\n')
