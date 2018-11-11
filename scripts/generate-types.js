2818dd593fe7fb199c65ae30d6ef2a97fd24b285
Gregor <gregor@martynus.net>
2018-11-10 20:26:03 -0800

    build(typescript): generate directly from @octokit/routes

--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -3,6 +3,7 @@ module.exports = generateTypes
 const { readFileSync, writeFileSync } = require('fs')
 const { join: pathJoin } = require('path')
 
+const _ = require('lodash')
 const debug = require('debug')('octokit:rest')
 const Mustache = require('mustache')
 const upperFirst = require('lodash/upperFirst')
@@ -11,7 +12,7 @@ const set = require('lodash/set')
 const TypeWriter = require('@gimenete/type-writer')
 const prettier = require('prettier')
 
-const ROUTES = require('./routes-for-api-docs.json')
+const ROUTES = require('@octokit/routes')
 
 const typeMap = {
   integer: 'number',
@@ -55,10 +56,6 @@ function isLocalParam (name) {
   return !isGlobalParam(name)
 }
 
-function entries (object) {
-  return Object.keys(object).map((key) => [key, object[key]])
-}
-
 function toCombineParams (params, param) {
   return params
     .concat(parameterize(param))
@@ -79,6 +76,10 @@ function jsdoc (description) {
   return description && '/**\n' + description.split('\n').map(str => '* ' + str) + '\n*/'
 }
 
+function normalize (methodName) {
+  return _.camelCase(methodName.replace(/^edit/, 'update'))
+}
+
 function generateTypes (languageName, templateFile, outputFile) {
   const templatePath = pathJoin(__dirname, 'templates', templateFile)
   const template = readFileSync(templatePath, 'utf8')
@@ -87,75 +88,81 @@ function generateTypes (languageName, templateFile, outputFile) {
   debug(`Generating ${languageName} types...`)
 
   const childParams = {}
-  const namespaces = Object.keys(ROUTES).reduce((namespaces, namespace) => {
-    const methods = entries(ROUTES[namespace]).reduce((methods, entry) => {
-      const unionTypeNames = Object.keys(entry[1].params)
-        .filter(isGlobalParam)
-        .reduce((params, name) => {
-          return params.concat(pascalcase(name.slice(1)))
-        }, [])
+  const namespaces = Object.keys(ROUTES)
+    .filter(namespace => namespace !== 'scim')
+    .reduce((namespaces, namespace) => {
+      const methods = ROUTES[namespace].reduce((methods, entry) => {
+        if (/legacy$/.test(entry.idName)) {
+          return
+        }
 
-      const namespacedParamsName = pascalcase(`${namespace}-${entry[0]}Params`)
-      const ownParams = entry[1].params
-        .filter((param) => isLocalParam(param.name))
-        .reduce(toCombineParams, [])
-        .map(toParamAlias)
-        // handle "object" & "object[]" types
-        .map(param => {
-          if (param.type === 'object' || param.type === 'object[]') {
-            const childParamsName = pascalcase(`${namespacedParamsName}.${param.key}`)
-            param.type = param.type.replace('object', childParamsName)
+        const unionTypeNames = Object.keys(entry.params)
+          .filter(isGlobalParam)
+          .reduce((params, name) => {
+            return params.concat(pascalcase(name.slice(1)))
+          }, [])
 
-            if (!childParams[childParamsName]) {
-              childParams[childParamsName] = {}
+        const methodName = normalize(entry.idName)
+        const namespacedParamsName = pascalcase(`${namespace}-${methodName}Params`)
+        const ownParams = entry.params
+          .filter((param) => isLocalParam(param.name))
+          .reduce(toCombineParams, [])
+          .map(toParamAlias)
+          // handle "object" & "object[]" types
+          .map(param => {
+            if (param.type === 'object' || param.type === 'object[]') {
+              const childParamsName = pascalcase(`${namespacedParamsName}.${param.key}`)
+              param.type = param.type.replace('object', childParamsName)
+
+              if (!childParams[childParamsName]) {
+                childParams[childParamsName] = {}
+              }
             }
-          }
 
-          if (!/\./.test(param.key)) {
-            return param
-          }
+            if (!/\./.test(param.key)) {
+              return param
+            }
 
-          const childKey = param.key.split('.').pop()
-          const parentKey = param.key.replace(/\.[^.]+$/, '')
+            const childKey = param.key.split('.').pop()
+            const parentKey = param.key.replace(/\.[^.]+$/, '')
 
-          param.key = childKey
+            param.key = childKey
 
-          const childParamsName = pascalcase(`${namespacedParamsName}.${parentKey}`)
-          set(childParams, `${childParamsName}.${childKey}`, param)
+            const childParamsName = pascalcase(`${namespacedParamsName}.${parentKey}`)
+            set(childParams, `${childParamsName}.${childKey}`, param)
+          })
+          .filter(Boolean)
+
+        const hasParams = unionTypeNames.length > 0 || ownParams.length > 0
+
+        let paramTypeName = hasParams
+          ? namespacedParamsName
+          : pascalcase('EmptyParams')
+
+        let responseType = 'Github.AnyResponse'
+        if (entry.responses) {
+          const typeName = 'Github.' + typeWriter.add(entry.responses.map(response => response.body || {}), {
+            rootTypeName: pascalcase(`${namespace}-${entry.idName}Response`)
+          })
+          responseType = 'Github.Response<' + typeName + '>'
+        }
+
+        return methods.concat({
+          method: methodName,
+          paramTypeName,
+          unionTypeNames: unionTypeNames.length > 0 && unionTypeNames,
+          ownParams: ownParams.length > 0 && { params: ownParams },
+          exclude: !hasParams,
+          responseType,
+          jsdoc: jsdoc(entry.description)
         })
-        .filter(Boolean)
+      }, [])
 
-      const hasParams = unionTypeNames.length > 0 || ownParams.length > 0
-
-      const methodName = camelcase(entry[0])
-      let paramTypeName = hasParams
-        ? namespacedParamsName
-        : pascalcase('EmptyParams')
-
-      let responseType = 'Github.AnyResponse'
-      if (entry[1].responses) {
-        const typeName = 'Github.' + typeWriter.add(entry[1].responses.map(response => response.body || {}), {
-          rootTypeName: pascalcase(`${namespace}-${entry[0]}Response`)
-        })
-        responseType = 'Github.Response<' + typeName + '>'
-      }
-
-      return methods.concat({
-        method: methodName,
-        paramTypeName,
-        unionTypeNames: unionTypeNames.length > 0 && unionTypeNames,
-        ownParams: ownParams.length > 0 && { params: ownParams },
-        exclude: !hasParams,
-        responseType,
-        jsdoc: jsdoc(entry[1].description)
+      return namespaces.concat({
+        namespace: camelcase(namespace),
+        methods
       })
     }, [])
-
-    return namespaces.concat({
-      namespace: camelcase(namespace),
-      methods
-    })
-  }, [])
 
   const body = Mustache.render(template, {
     responseTypes: typeWriter.generate('typescript'),
