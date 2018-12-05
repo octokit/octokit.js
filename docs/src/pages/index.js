42e77620a11b95d328d8b4aa3d5da78f16d1f7af
Gregor <gregor@martynus.net>
2018-12-05 14:08:46 -0800

    docs(website): API & guides

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 export default () => (
   <Layout>
     <div>
-      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <h1>Home</h1>
       <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
+        Check out our <Link to='guides'>guides</Link> or <Link to='api'>API</Link>.
       </p>
     </div>
   </Layout>
