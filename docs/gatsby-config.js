68ba4d1bd9a1eb144c5b3dd1bc1e59dc7bafcdc2
Gregor <gregor@martynus.net>
2018-12-07 14:51:03 -0800

    docs(website): markdown guides with index page

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,9 +1,20 @@
 module.exports = {
+  siteMetadata: {
+    title: `Octokit.js`
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
+        pathToConfigModule: `src/utils/typography`
       }
     }
   ]
