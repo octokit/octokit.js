265c911a519a6215c5c5037e7caa0a34e2de8e14
Jim Thoburn <jim@jimthoburn.com>
2018-12-07 16:00:32 -0800

    docs(website): Add CSS layout

---
+++ docs/src/components/guides-list.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StaticQuery, Link, graphql } from 'gatsby'
+
+export default ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark {
+          totalCount
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <nav>
+        <h1>Guides</h1>
+        <ol>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <li key={node.id}>
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+          </li>
+        ))}
+        </ol>
+      </nav>
+    )}
+  />
+)
+
--- docs/src/templates/guide.js
+++ docs/src/templates/guide.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import GuidesList from '../components/guides-list'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
+      <main>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      </main>
+      <GuidesList />
     </Layout>
   )
 }
