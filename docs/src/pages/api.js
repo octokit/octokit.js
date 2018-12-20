d510850e733d02be7d5e9707e706278174aa7763
Jim Thoburn <jim@jimthoburn.com>
2018-12-20 15:59:49 -0800

    docs(website): Remove the CSS grids for now
    …and move the nav to the top of the document.

--- docs/src/pages/api.js
+++ docs/src/pages/api.js
@@ -9,6 +9,36 @@ export default ({ data }) => (
   <Layout>
     <main className={apiStyles.container}>
       <h1>API</h1>
+      <nav>
+        <details>
+          <summary>
+            <h1>Menu</h1>
+          </summary>
+          <ol>
+            {data.staticMethods.edges.map(({ node }) => {
+              return <li key={node.id}>
+                <Link to={`/api#octokit-${node.fields.idName}`}>{node.frontmatter.title}</Link>
+              </li>
+            })}
+            {data.endpointScopes.edges.map(({ node }) => {
+              return <li key={node.id}>
+                <details>
+                  <summary>
+                    <Link to={`/api#${node.id}`}>{node.name}</Link>
+                  </summary>
+                  <ol>
+                    {node.methods.map((method) => {
+                      return <li key={method.id}>
+                        <Link to={`/api#${method.id}`}>{method.name}</Link>
+                      </li>
+                    })}
+                  </ol>
+                </details>
+              </li>
+            })}
+          </ol>
+        </details>
+      </nav>
       {data.staticMethods.edges.map(({ node }) => {
         return <>
           <h2>{node.frontmatter.title}</h2>
@@ -17,62 +47,38 @@ export default ({ data }) => (
       })}
       {data.endpointScopes.edges.map(({ node }) => {
         return <>
-          <h2>{node.name}</h2>
+          <h2 id={node.id}>{node.name}</h2>
           {node.methods.map(method => {
             return <>
-              <h3>{method.name}</h3>
+              <h3 id={method.id}>{method.name}</h3>
               <div dangerouslySetInnerHTML={{ __html: marked(method.description) }} />
               <h4>Parameters</h4>
-              <table>
-                <thead>
-                  <tr>
-                    <th>name</th>
-                    <th>required</th>
-                    <th>description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {method.params.map(param => {
-                    return <tr>
-                      <td>{param.name}</td>
-                      <td>{param.required ? 'yes' : 'no'}</td>
-                      <td>{param.description}</td>
+              <div className={apiStyles.table}>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>name</th>
+                      <th>required</th>
+                      <th>description</th>
                     </tr>
-                  })}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {method.params.map(param => {
+                      return <tr>
+                        <td>{param.name}</td>
+                        <td>{param.required ? 'yes' : 'no'}</td>
+                        <td>{param.description}</td>
+                      </tr>
+                    })}
+                  </tbody>
+                </table>
+              </div>
               <pre><code>{method.example}</code></pre>
             </>
           })}
         </>
       })}
     </main>
-    <nav>
-      <h1>API</h1>
-      <ol>
-        {data.staticMethods.edges.map(({ node }) => {
-          return <li key={node.id}>
-            <Link to={`/api#octokit-${node.fields.idName}`}>{node.frontmatter.title}</Link>
-          </li>
-        })}
-        {data.endpointScopes.edges.map(({ node }) => {
-          return <li key={node.id}>
-            <details>
-              <summary>
-                <Link to={`/api#${node.id}`}>{node.name}</Link>
-              </summary>
-              <ol>
-                {node.methods.map((method) => {
-                  return <li key={method.id}>
-                    <Link to={`/api#${method.id}`}>{method.name}</Link>
-                  </li>
-                })}
-              </ol>
-            </details>
-          </li>
-        })}
-      </ol>
-    </nav>
   </Layout>
 )
 
