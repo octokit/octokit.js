fb38885ed4ad197f682bb6d7e879792a6a9d5189
Gregor <gregor@martynus.net>
2019-01-17 10:18:18 -0800

    feat: two-factor authentication

--- plugins/authentication/index.js
+++ plugins/authentication/index.js
@@ -2,11 +2,14 @@ module.exports = authenticationPlugin
 
 const authenticate = require('./authenticate')
 const beforeRequest = require('./before-request')
+const requestError = require('./request-error')
 
 function authenticationPlugin (octokit) {
   const state = {
+    octokit,
     auth: false
   }
   octokit.authenticate = authenticate.bind(null, state)
   octokit.hook.before('request', beforeRequest.bind(null, state))
+  octokit.hook.error('request', requestError.bind(null, state))
 }
