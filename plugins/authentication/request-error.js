d29ec5aca70c55a04cdccb020ac0cfaa3f256ebd
Gregor <gregor@martynus.net>
2019-01-19 11:05:12 -0800

    fix: handle invalid otp for 2FA
    When the `on2fa()` method returns an invalid otp, octokit would end up in an infinite loop.

--- plugins/authentication/request-error.js
+++ plugins/authentication/request-error.js
@@ -3,9 +3,14 @@ module.exports = authenticationRequestError
 const HttpError = require('@octokit/request/lib/http-error')
 
 function authenticationRequestError (state, error, options) {
+  const otpRequired = /required/.test(error.headers['x-github-otp'] || '')
   // handle "2FA required" error only
-  if (error.status !== 401 || !/required/.test(error.headers['x-github-otp'] || '')) {
+  if (error.status !== 401 || !otpRequired) {
     throw error
+  }
+
+  if (error.status === 401 && otpRequired && error.request && error.request.headers['x-github-otp']) {
+    throw new HttpError('Invalid one-time password for two-factor authentication', 401, error.headers, options)
   }
 
   if (typeof state.auth.on2fa !== 'function') {
