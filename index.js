kphed/rest.js

feat: `log` client option

index.js
@@ -1,6 +1,7 @@
 const Octokit = require('./lib/core')
 
 const CORE_PLUGINS = [
+  require('./plugins/debug'),
   require('./plugins/authentication'),
   require('./plugins/pagination'),
   require('./plugins/register-endpoints'),

octokit/octokit.js

index.js
@@ -1,6 +1,7 @@
 const Octokit = require('./lib/core')
 
 const CORE_PLUGINS = [
+  require('./plugins/log'),
   require('./plugins/authentication-deprecated'), // deprecated: remove in v17
   require('./plugins/authentication'),
   require('./plugins/pagination'),