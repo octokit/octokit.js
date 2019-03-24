75f7272c9a66c06d781976abaed045f8429c2891
Gregor Martynus <gregor@martynus.net>
2019-03-24 15:20:59 -0700

    docs(website): Improved search (#27)

--- docs/src/components/layout.js
+++ docs/src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StaticQuery, Link } from 'gatsby'
 
 import layoutStyles from './layout.module.css'
@@ -11,32 +11,56 @@ const ListLink = props => (
   </li>
 )
 
-export default ({ children }) => (
-  <div className={layoutStyles.container}>
-    <header className={layoutStyles.header}>
-      <Link to='/' style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>octokit.js</h3>
-      </Link>
+class Layout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasSearchQuery: false
+    }
+    this.onSearch = this.onSearch.bind(this)
+  }
 
-      <StaticQuery
-        query={graphql`
-          query SearchIndexQuery {
-            siteSearchIndex {
-              index
+  onSearch(query) {
+    this.setState({
+      hasSearchQuery: !!query.trim()
+    })
+  }
+
+  render() {
+    return <div>
+      <header className={layoutStyles.header}>
+        <Link to='/' style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>octokit/rest.js</h3>
+        </Link>
+
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to='/'>Home</ListLink>
+          <ListLink to='/guides/'>Guides</ListLink>
+          <ListLink to='/api/'>API</ListLink>
+        </ul>
+
+        <StaticQuery
+          query={graphql`
+            query SearchIndexQuery {
+              siteSearchIndex {
+                index
+              }
             }
-          }
-        `}
-        render={data => (
-          <Search searchIndex={data.siteSearchIndex.index} />
-        )}
-      />
+          `}
+          render={data => (
+            <Search onSearch={this.onSearch} searchIndex={data.siteSearchIndex.index} />
+          )}
+        />
+      </header>
 
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to='/'>Home</ListLink>
-        <ListLink to='/guides/'>Guides</ListLink>
-        <ListLink to='/api/'>API</ListLink>
-      </ul>
-    </header>
-    {children}
-  </div>
+      <div className={this.state.hasSearchQuery ? [layoutStyles.container, layoutStyles.hidden].join(' ') : layoutStyles.container}>
+        {this.props.children}
+      </div>
+    </div>
+  }
+}
+
+
+export default ({ children }) => (
+  <Layout children={children}></Layout>
 )
--- docs/src/components/search.js
+++ docs/src/components/search.js
@@ -10,18 +10,38 @@ export default class Search extends Component {
       query: ``,
       results: [],
     }
+    this.reset = this.reset.bind(this)
   }
 
   render() {
     return (
       <div>
         <input type="text" value={this.state.query} onChange={this.search} />
-        <ul>
-          {this.state.results.map(page => (
-            <li key={page.id}>
-              <Link to={"/" + page.slug}>{page.title}</Link>
+        <ul onClick={this.reset}>
+          {this.state.results.slice(0, 3).map(page => {
+            if (page.type === 'API method') {
+              return <li key={page.id}>
+                <Link to={page.slug} onClick={this.reset}>
+                  <strong>{page.name}</strong> <small>(<code>{page.route}</code>)</small><br />
+                  <code>octokit.{page.method}</code>
+                </Link>
+              </li>
+            }
+
+            if (page.type === 'API') {
+              return <li key={page.id}>
+                <Link to={page.slug} onClick={this.reset}>
+                  <strong>{page.title}</strong> (API)
+                </Link>
+              </li>
+            }
+
+            return <li key={page.id}>
+              <Link to={page.slug} onClick={this.reset}>
+                <strong>{page.title}</strong> (Guide)
+              </Link>
             </li>
-          ))}
+          })}
         </ul>
       </div>
     )
@@ -32,17 +52,37 @@ export default class Search extends Component {
       : // Create an elastic lunr index and hydrate with graphql query results
         Index.load(this.props.searchIndex)
 
+
+
   search = evt => {
     const query = evt.target.value
+
+    this.props.onSearch(query)
     this.index = this.getOrCreateIndex()
     this.setState({
       query,
       // Query the index with search string to get an [] of IDs
       results: this.index
-        // .search(query, { expand: true })
-        .search(query, {})
+        .search(query, {
+          expand: true,
+          fields: {
+            title: {boost: 3},
+            name: {boost: 2 },
+            scope: {boost: 2 },
+            route: {boost: 1 },
+            method: {boost: 1 }
+          }
+        })
         // Map over each ID and return the full document
         .map(({ ref }) => this.index.documentStore.getDoc(ref)),
     })
+  }
+
+  reset = evt => {
+    this.setState({
+      query: '',
+      results: []
+    })
+    this.props.onSearch('')
   }
 }
