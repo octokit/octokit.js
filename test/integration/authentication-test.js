75b05a43a5017703c50b591dc96f6696446333c3
Gregor <gregor@martynus.net>
2019-03-28 17:29:49 -0700

    test: `options.auth` set to string without basic/token/bearer prefix

--- test/integration/authentication-test.js
+++ test/integration/authentication-test.js
@@ -1,4 +1,5 @@
 const nock = require('nock')
+const btoa = require('btoa-lite')
 
 const Octokit = require('../..')
 
@@ -348,6 +349,57 @@ describe('authentication', () => {
     return octokit.request('/')
 
       .catch(() => {})
+  })
+
+  it('options.auth=token without prefix', () => {
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'token abc4567'
+      }
+    })
+      .get('/')
+      .reply(200, {})
+
+    const octokit = new Octokit({
+      baseUrl: 'https://authentication-test-host.com',
+      auth: 'abc4567'
+    })
+
+    return octokit.request('/')
+  })
+
+  it('options.auth=basic without prefix', () => {
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'basic Zm9vLWJhcjpzZWNyZXQ='
+      }
+    })
+      .get('/')
+      .reply(200, {})
+
+    const octokit = new Octokit({
+      baseUrl: 'https://authentication-test-host.com',
+      auth: btoa('foo-bar:secret')
+    })
+
+    return octokit.request('/')
+  })
+
+  it('options.auth=bearer without prefix', () => {
+    nock('https://authentication-test-host.com', {
+      reqheaders: {
+        authorization: 'bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTM4MTkzMTIsImV4cCI6MTU1MzgxOTM3MiwiaXNzIjoxfQ.etiSZ4LFQZ8tiMGJVqKDoGn8hxMCgwL4iLvU5xBUqbAPr4pbk_jJZmMQjuxTlOnRxq4e7NouTizGCdfohRMb3R1mpLzGPzOH9_jqSA_BWYxolsRP_WDSjuNcw6nSxrPRueMVRBKFHrqcTOZJej0djRB5pI61hDZJ_-DGtiOIFexlK3iuVKaqBkvJS5-TbTekGuipJ652g06gXuz-l8i0nHiFJldcuIruwn28hTUrjgtPbjHdSBVn_QQLKc2Fhij8OrhcGqp_D_fvb_KovVmf1X6yWiwXV5VXqWARS-JGD9JTAr2495ZlLV_E4WPxdDpz1jl6XS9HUhMuwBpaCOuipw'
+      }
+    })
+      .get('/')
+      .reply(200, {})
+
+    const octokit = new Octokit({
+      baseUrl: 'https://authentication-test-host.com',
+      auth: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTM4MTkzMTIsImV4cCI6MTU1MzgxOTM3MiwiaXNzIjoxfQ.etiSZ4LFQZ8tiMGJVqKDoGn8hxMCgwL4iLvU5xBUqbAPr4pbk_jJZmMQjuxTlOnRxq4e7NouTizGCdfohRMb3R1mpLzGPzOH9_jqSA_BWYxolsRP_WDSjuNcw6nSxrPRueMVRBKFHrqcTOZJej0djRB5pI61hDZJ_-DGtiOIFexlK3iuVKaqBkvJS5-TbTekGuipJ652g06gXuz-l8i0nHiFJldcuIruwn28hTUrjgtPbjHdSBVn_QQLKc2Fhij8OrhcGqp_D_fvb_KovVmf1X6yWiwXV5VXqWARS-JGD9JTAr2495ZlLV_E4WPxdDpz1jl6XS9HUhMuwBpaCOuipw'
+    })
+
+    return octokit.request('/')
   })
 
   it('invalid auth errors', () => {
