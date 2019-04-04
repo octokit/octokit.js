27e29772726d57c468b0ad8b88bc9e9d00d6617a
Gregor <gregor@martynus.net>
2019-04-04 11:07:36 -0700

    fix: .paginate() with results namespace (GET /installation/repositories, single page response)

--- plugins/pagination/iterator.js
+++ plugins/pagination/iterator.js
@@ -1,5 +1,7 @@
 module.exports = iterator
 
+const normalizePaginatedListResponse = require('./normalize-paginated-list-response')
+
 function iterator (octokit, options) {
   const headers = options.headers
   let url = octokit.request.endpoint(options).url
@@ -14,6 +16,8 @@ function iterator (octokit, options) {
         return octokit.request({ url, headers })
 
           .then((response) => {
+            normalizePaginatedListResponse(octokit, url, response)
+
             // `response.headers.link` format:
             // '<https://api.github.com/users/aseemk/followers?page=2>; rel="next", <https://api.github.com/users/aseemk/followers?page=2>; rel="last"'
             // sets `url` to undefined if "next" URL is not present or `link` header is not set
--- plugins/pagination/paginate.js
+++ plugins/pagination/paginate.js
@@ -1,12 +1,5 @@
 module.exports = paginate
 
-const Deprecation = require('deprecation')
-const once = require('once')
-
-const deprecateIncompleteResults = once((log, deprecation) => log.warn(deprecation))
-const deprecateTotalCount = once((log, deprecation) => log.warn(deprecation))
-const deprecateNamespace = once((log, deprecation) => log.warn(deprecation))
-
 const iterator = require('./iterator')
 
 function paginate (octokit, route, options, mapFn) {
@@ -28,51 +21,6 @@ function gather (octokit, results, iterator, mapFn) {
       let earlyExit = false
       function done () {
         earlyExit = true
-      }
-
-      // normalize list responses with { total_count, incomplete_results, items } keys
-      // https://github.com/octokit/rest.js/issues/1283
-      if ('total_count' in result.value.data && result.value.headers.link) {
-        const incompleteResults = result.value.data.incomplete_results
-        const repositorySelection = result.value.data.repository_selection
-        const totalCount = result.value.data.total_count
-        delete result.value.data.incomplete_results
-        delete result.value.data.repository_selection
-        delete result.value.data.total_count
-
-        const namespaceKey = Object.keys(result.value.data)[0]
-
-        result.value.data = result.value.data[namespaceKey]
-
-        Object.defineProperty(result.value.data, namespaceKey, {
-          get () {
-            deprecateNamespace(octokit.log, new Deprecation(`[@octokit/rest] "result.data.${namespaceKey}" is deprecated. Use "result.data" instead`))
-            return result.value.data
-          }
-        })
-
-        if (typeof incompleteResults !== 'undefined') {
-          Object.defineProperty(result.value.data, 'incomplete_results', {
-            get () {
-              deprecateIncompleteResults(octokit.log, new Deprecation('[@octokit/rest] "result.data.incomplete_results" is deprecated.'))
-              return incompleteResults
-            }
-          })
-        }
-
-        Object.defineProperty(result.value.data, 'repository_selection', {
-          get () {
-            deprecateTotalCount(octokit.log, new Deprecation('[@octokit/rest] "result.data.repository_selection" is deprecated.'))
-            return repositorySelection
-          }
-        })
-
-        Object.defineProperty(result.value.data, 'total_count', {
-          get () {
-            deprecateTotalCount(octokit.log, new Deprecation('[@octokit/rest] "result.data.total_count" is deprecated.'))
-            return totalCount
-          }
-        })
       }
 
       results = results.concat(mapFn ? mapFn(result.value, done) : result.value.data)
