dd0ca3ad6f5fec201b591e1f7dc47bbc3b30c589
Jim Thoburn <jim@jimthoburn.com>
2019-04-07 16:55:07 -0700

    docs(website): Fix bug where “no results” message appears too soon

--- docs/src/components/search.js
+++ docs/src/components/search.js
@@ -27,7 +27,7 @@ export default class Search extends Component {
           <input type="search" value={this.state.query} onFocus={this.onFocus} onBlur={this.onBlur} onChange={this.search} placeholder="search" />
         </label>
         <ul onClick={this.reset} class="results">
-          {this.state.results.length === 0 ? (<li>No results found.</li>) : ""}
+          {this.state.query && this.state.results.length === 0 ? (<li>No results found.</li>) : ""}
 
           {this.state.results.map(page => {
             if (page.type === 'API method') {
