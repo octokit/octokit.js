b1cae867410358b3fd4714d1588d6cd4f6cfa00f
Gregor <gregor@martynus.net>
2019-04-15 16:49:40 -0700

    build(generate-routes): remove workarounds that have been moved to octokit/routes

--- scripts/lib/get-routes.js
+++ scripts/lib/get-routes.js
@@ -35,48 +35,11 @@ function getRoutes () {
 
     // normalize idName
     endpoints.forEach(normalize)
-
-    // handle some exceptions. TODO: move this into @octokit/routes
-    endpoints.forEach(endpoint => {
-      // exception for uploadReleaseAssets which passes parameters as header values
-      // see https://github.com/octokit/rest.js/pull/1043
-      if (endpoint.idName === 'uploadReleaseAsset') {
-        const contentLengthParam = endpoint.params.find(param => param.name === 'Content-Length')
-        const contentTypeParam = endpoint.params.find(param => param.name === 'Content-Type')
-        const indexes = endpoint.params.reduce((result, param, i) => {
-          if (['Content-Length', 'Content-Type'].includes(param.name)) {
-            result.unshift(i)
-          }
-
-          return result
-        }, [])
-        indexes.forEach(i => endpoint.params.splice(i, 1))
-        endpoint.params.unshift(
-          Object.assign(contentLengthParam, { name: 'headers.content-length' }),
-          Object.assign(contentTypeParam, { name: 'headers.content-type' }),
-          {
-            name: 'headers',
-            location: 'headers',
-            required: true,
-            type: 'object',
-            description: 'Request headers containing `content-type` and `content-length`'
-          }
-        )
-      }
-
-      // exception for markdown.renderRaw which requires a content-type header
-      // see https://github.com/octokit/rest.js/pull/1043
-      if (endpoint.idName === 'renderRaw') {
-        endpoint.headers = {
-          'content-type': 'text/plain; charset=utf-8'
-        }
-      }
-    })
   })
 
   return ROUTES
 }
 
 function normalize (endpoint) {
-  endpoint.idName = camelCase(endpoint.idName.replace(/^edit/, 'update'))
+  endpoint.idName = camelCase(endpoint.idName)
 }
