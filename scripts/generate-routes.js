dd563eedce2033091e22e9ea5ff8ce49e5302f72
Gregor <gregor@martynus.net>
2019-05-24 16:43:28 -0700

    WIP endpoint deprecations

--- scripts/generate-routes.js
+++ scripts/generate-routes.js
@@ -10,6 +10,67 @@ const ROUTES_PATH = join(__dirname, '..', 'plugins', 'rest-api-endpoints', 'rout
 
 const ROUTES = {}
 
+function definitionToEndpoint ({ method, url }, definition) {
+  const scope = camelCase(definition.tags[0])
+  const idName = camelCase(definition.operationId.substr(definition.tags[0].length + 1))
+
+  if (!ROUTES[scope]) {
+    ROUTES[scope] = {}
+  }
+
+  const params = {}
+
+  // URL, headers and query params
+  definition.parameters
+    .forEach(param => {
+      if (param.in === 'header') {
+        return
+      }
+
+      params[param.name] = {
+        type: param.schema.type,
+        enum: param.schema.enum
+      }
+
+      if (param.required) {
+        params[param.name].required = true
+      }
+    })
+
+  // request body params
+  if (definition.requestBody) {
+    const requiredParams = definition.requestBody.content['application/json'].schema.required
+    Object.entries(definition.requestBody.content['application/json'].schema.properties)
+      .forEach(([param, definition]) => {
+        params[param] = {
+          type: definition.type,
+          enum: definition.enum
+        }
+
+        if (requiredParams && requiredParams.includes(param)) {
+          params[param].required = true
+        }
+      })
+  }
+
+  const headerParameters = definition.parameters
+    .filter(param => param.in === 'header')
+  const acceptHeader = headerParameters.find(param => param.name === 'accept')
+
+  ROUTES[scope][idName] = {
+    method: method.toUpperCase(),
+    url: url.replace(/\{([^}]+)\}/g, ':$1'),
+    params,
+    deprecated: definition.deprecated
+  }
+
+  if (acceptHeader.required) {
+    ROUTES[scope][idName].headers = {
+      accept: acceptHeader.schema.default
+    }
+  }
+}
+
 jsonSchemaRefParser.dereference(require.resolve('@octokit/routes'))
   .then(schema => {
     for (const [url, definitions] of Object.entries(schema.paths)) {
@@ -23,215 +84,25 @@ jsonSchemaRefParser.dereference(require.resolve('@octokit/routes'))
           continue
         }
 
-        const scope = camelCase(definition.tags[0])
-        const idName = camelCase(definition.operationId.substr(definition.tags[0].length + 1))
+        definitionToEndpoint({ method, url }, definition)
 
-        if (!ROUTES[scope]) {
-          ROUTES[scope] = {}
-        }
-
-        const params = {}
-
-        // URL, headers and query params
-        definition.parameters
-          .forEach(param => {
-            if (param.in === 'header') {
-              return
-            }
-
-            params[param.name] = {
-              type: param.schema.type,
-              enum: param.schema.enum
-            }
-
-            if (param.required) {
-              params[param.name].required = true
-            }
-          })
-
-        // request body params
-        if (definition.requestBody) {
-          const requiredParams = definition.requestBody.content['application/json'].schema.required
-          Object.entries(definition.requestBody.content['application/json'].schema.properties)
-            .forEach(([param, definition]) => {
-              params[param] = {
-                type: definition.type,
-                enum: definition.enum
-              }
-
-              if (requiredParams && requiredParams.includes(param)) {
-                params[param].required = true
-              }
-            })
-        }
-
-        const headerParameters = definition.parameters
-          .filter(param => param.in === 'header')
-        const acceptHeader = headerParameters.find(param => param.name === 'accept')
-
-        ROUTES[scope][idName] = {
-          method: method.toUpperCase(),
-          url: url.replace(/\{([^}]+)\}/g, ':$1'),
-          params
-        }
-
-        if (acceptHeader.required) {
-          ROUTES[scope][idName].headers = {
-            accept: acceptHeader.schema.default
+        definition['x-changes'].forEach(change => {
+          if (change.type !== 'idName') {
+            return
           }
-        }
 
-        // {
-        //   summary: 'List issues for a repository',
-        //   description: '**Note**: GitHub\'s REST API v3 considers every pull request an issue, but not every issue is a pull request. For this reason, "Issues" endpoints may return both issues and pull requests in the response. You can identify pull requests by the `pull_request` key.\n\nBe aware that the `id` of a pull request returned from "Issues" endpoints will be an _issue id_. To find out the pull request id, use the "[List pull requests](https://developer.github.com/v3/pulls/#list-pull-requests)" endpoint.\n\n',
-        //   operationId: 'issues-list-for-repo',
-        //   tags: [
-        //     'issues'
-        //   ],
-        //   externalDocs: {
-        //     description: 'API method documentation',
-        //     url: 'https://developer.github.com/v3/issues/#list-issues-for-a-repository'
-        //   },
-        //   parameters: [
-        //     {
-        //       name: 'owner',
-        //       'in': 'path',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: true,
-        //       description: 'owner parameter'
-        //     },
-        //     {
-        //       name: 'repo',
-        //       'in': 'path',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: true,
-        //       description: 'repo parameter'
-        //     },
-        //     {
-        //       name: 'milestone',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'If an `integer` is passed, it should refer to a milestone by its `number` field. If the string `*` is passed, issues with any milestone are accepted. If the string `none` is passed, issues without milestones are returned.'
-        //     },
-        //     {
-        //       name: 'state',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'Indicates the state of the issues to return. Can be either `open`, `closed`, or `all`.'
-        //     },
-        //     {
-        //       name: 'assignee',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'Can be the name of a user. Pass in `none` for issues with no assigned user, and `*` for issues assigned to any user.'
-        //     },
-        //     {
-        //       name: 'creator',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'The user that created the issue.'
-        //     },
-        //     {
-        //       name: 'mentioned',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'A user that\'s mentioned in the issue.'
-        //     },
-        //     {
-        //       name: 'labels',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'A list of comma separated label names. Example: `bug,ui,@high`'
-        //     },
-        //     {
-        //       name: 'sort',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'What to sort results by. Can be either `created`, `updated`, `comments`.'
-        //     },
-        //     {
-        //       name: 'direction',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'The direction of the sort. Can be either `asc` or `desc`.'
-        //     },
-        //     {
-        //       name: 'since',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'string'
-        //       },
-        //       required: false,
-        //       description: 'Only issues updated at or after this time are returned. This is a timestamp in [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) format: `YYYY-MM-DDTHH:MM:SSZ`.'
-        //     },
-        //     {
-        //       name: 'per_page',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'integer'
-        //       },
-        //       required: false,
-        //       description: 'Results per page (max 100)'
-        //     },
-        //     {
-        //       name: 'page',
-        //       'in': 'query',
-        //       schema: {
-        //         type: 'integer'
-        //       },
-        //       required: false,
-        //       description: 'Page number of the results to fetch.'
-        //     }
-        //   ],
-        //   'x-code-samples': [
-        //     {
-        //       lang: 'Shell',
-        //       source: 'curl \\\n  -H"Accept: application/vnd.github.v3+json" \\\n  https://developer.github.com/repos/octocat/:repo/issues'
-        //     },
-        //     {
-        //       lang: 'JS',
-        //       source: 'octokit.issues.get({\n  owner: \'octocat\',\n  repo: \'hello-world\'\n})'
-        //     }
-        //   ],
-        //   responses: {
-        //     '200': {
-        //       description: 'response',
-        //       content: {
-        //         'application/json': {
-        //           schema: {}
-        //         }
-        //       }
-        //     }
-        //   }
-        // }
+          const scope = camelCase(definition.tags[0])
+          const before = camelCase(change.meta.before.idName)
+          const after = camelCase(change.meta.after.idName)
+
+          definitionToEndpoint(
+            { method, url },
+            Object.assign({}, definition, {
+              operationId: [definition.tags[0], change.meta.before.idName].join('-'),
+              deprecated: `octokit.${scope}.${before}() has been renamed to octokit.${scope}.${after}() (${change.date})`
+            })
+          )
+        })
       }
     }
   })
