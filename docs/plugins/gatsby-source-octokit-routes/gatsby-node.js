00b2c47c7eddbfde4cdae345741ffb9e153feb57
Gregor <gregor@martynus.net>
2019-05-30 21:53:33 -0700

    adapt gatsby-source-octokit-routes plugin to use with OpenAPI spec

--- docs/plugins/gatsby-source-octokit-routes/gatsby-node.js
+++ docs/plugins/gatsby-source-octokit-routes/gatsby-node.js
@@ -1,33 +1,124 @@
 const _ = require('lodash')
-const ROUTES = require('@octokit/routes')
+const jsonSchemaRefParser = require('json-schema-ref-parser')
+
+function schemaToParams (params, schema, prefix = '') {
+  Object.entries(schema.properties)
+    .forEach(([paramName, definition]) => {
+      const paramNameWithPrefix = prefix + paramName
+      const param = {
+        name: paramNameWithPrefix,
+        type: definition.type,
+        enum: definition.enum,
+        description: definition.description
+      }
+
+      params.push(param)
+
+      if (schema.required && schema.required.includes(paramName)) {
+        param.required = true
+      }
+
+      if (definition.nullable) {
+        param.allowNull = true
+      }
+
+      if (definition.pattern) {
+        param.validation = definition.pattern
+      }
+
+      // handle arrays
+      if (definition.type === 'array') {
+        param.type = definition.items.type + '[]'
+
+        if (definition.items.type === 'object') {
+          schemaToParams(params, definition.items, `${paramNameWithPrefix}[].`)
+        }
+      }
+
+      // handle objects
+      if (definition.type === 'object') {
+        schemaToParams(params, definition, `${paramNameWithPrefix}.`)
+      }
+    })
+}
 
 exports.sourceNodes = async ({ actions }) => {
   const { createNode } = actions
 
-  Object.keys(ROUTES).map(scope => {
-    const scopeId = `octokit-routes-${scope}`
-    const methods = []
+  const schema = await jsonSchemaRefParser.dereference(require.resolve('@octokit/routes'))
 
-    ROUTES[scope].forEach(endpoint => {
-      if (/legacy/.test(endpoint.idName)) {
-        return
+  let methodsByScope = {}
+  for (const [path, definitions] of Object.entries(schema.paths)) {
+    for (const [method, definition] of Object.entries(definitions)) {
+      // ignore legacy endpoints
+      if (definition['x-github'].legacy) {
+        continue
       }
 
-      const endpointId = `octokit-routes-${scope}-${_.kebabCase(endpoint.idName)}`
-      const paramNames = endpoint.params
+      if (definition['x-github'].githubCloudOnly) {
+        continue
+      }
+
+      const scope = definition.tags[0]
+      if (!methodsByScope[scope]) {
+        methodsByScope[scope] = []
+      }
+
+      const scopeId = `octokit-routes-${scope}`
+
+      const endpointId = `octokit-routes-${definition.operationId}`
+      const idName = definition.operationId.substr(scope.length + 1)
+      const paramNames = definition.parameters
         .filter(param => param.required)
-        .filter(param => !/\./.test(param.name))
         .map(param => param.name)
+
+      if (definition.requestBody) {
+        if (definition['x-github'].requestBodyParameterName) {
+          paramNames.push(definition['x-github'].requestBodyParameterName)
+        } else {
+          paramNames.push(...(definition.requestBody.content['application/json'].schema.required || []))
+        }
+      }
+
+      const params = []
+
+      if (definition.requestBody) {
+        if (definition['x-github'].requestBodyParameterName) {
+          const paramName = definition['x-github'].requestBodyParameterName
+          const contentType = Object.keys(definition.requestBody.content)[0]
+          const { schema } = definition.requestBody.content[contentType]
+          params[paramName] = {
+            type: schema.type,
+            mapTo: 'data'
+          }
+          if (schema.type === 'array') {
+            params[paramName].type = schema.items.type + '[]'
+          }
+
+          params[paramName].required = true
+        } else {
+          schemaToParams(params, definition.requestBody.content['application/json'].schema)
+        }
+      }
+
       const paramsString = paramNames.length ? `{
   ${paramNames.join(',\n  ')}
 }` : ''
-      const example = `octokit.${scope}.${_.camelCase(endpoint.idName)}(${paramsString})`
-      const method = {
-        id: endpointId,
-        scope,
-        ...endpoint,
-        example
+      const example = `octokit.${scope}.${_.camelCase(idName)}(${paramsString})`
+
+      const endpoint = {
+        name: definition.summary,
+        enabledForApps: null, // TODO
+        githubCloudOnly: definition['x-github'].githubCloudOnly,
+        method,
+        path,
+        previews: [], // TODO
+        params,
+        description: definition.description,
+        idName,
+        documentationUrl: definition.externalDocs.url
       }
+
       createNode({
         id: endpointId,
         parent: scopeId,
@@ -41,11 +132,19 @@ exports.sourceNodes = async ({ actions }) => {
           type: 'OctokitApiMethod'
         }
       })
-      methods.push(method)
-    })
+      methodsByScope[scope].push({
+        id: endpointId,
+        scope,
+        ...endpoint,
+        example
+      })
+    }
+  }
 
+  Object.keys(methodsByScope).sort().forEach(scope => {
+    const methods = methodsByScope[scope]
     createNode({
-      id: scopeId,
+      id: `octokit-routes-${scope}`,
       parent: null,
       children: methods.map(method => method.id),
       methods: methods,
