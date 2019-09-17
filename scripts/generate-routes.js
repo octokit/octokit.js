bfdabb2779c64e3c1a4101c770fea4b4ffc1790e
Gregor Martynus <gregor@martynus.net>
2019-09-17 11:41:59 -0700

    build: generate routes using https://octokit-routes-graphql-server.now.sh/

--- scripts/generate-routes.js
+++ scripts/generate-routes.js
@@ -1,10 +1,9 @@
 const { writeFileSync } = require("fs");
 const { join } = require("path");
 
-const camelCase = require("lodash.camelcase");
 const sortKeys = require("sort-keys");
+const { graphql } = require("@octokit/graphql");
 
-const ROUTES = require("./lib/get-routes")();
 const ROUTES_PATH = join(
   __dirname,
   "..",
@@ -15,86 +14,279 @@ const ROUTES_PATH = join(
 
 const newRoutes = {};
 
-const endpoints = Object.keys(ROUTES).reduce((result, scope) => {
-  const scopeEndpoints = ROUTES[scope];
-  scopeEndpoints.forEach(endpoint => {
-    endpoint.scope = scope;
-  });
-  return result.concat(scopeEndpoints);
-}, []);
+generateRoutes();
 
-endpoints.forEach(endpoint => {
-  const scope = endpoint.scope;
-
-  if (!newRoutes[scope]) {
-    newRoutes[scope] = {};
-  }
-
-  const idName = endpoint.idName;
-
-  // new route
-  newRoutes[scope][idName] = {
-    method: endpoint.method,
-    headers: endpoint.headers,
-    params: endpoint.params.reduce((result, param) => {
-      result[param.name] = {
-        type: param.type
-      };
-      if (param.allowNull) {
-        result[param.name].allowNull = true;
-      }
-      if (param.required) {
-        result[param.name].required = true;
-      }
-      if (param.mapTo) {
-        result[param.name].mapTo =
-          param.mapTo === "input" ? "data" : param.mapTo;
-
-        if ((result[param.name].mapTo === "data") === param.name) {
-          delete result[param.name].mapTo;
+async function generateRoutes() {
+  const { endpoints } = await graphql(
+    `
+      {
+        endpoints(filter: { isLegacy: false, isGithubCloudOnly: false }) {
+          isDeprecated
+          documentationUrl
+          scope(format: CAMELCASE)
+          id(format: CAMELCASE)
+          method
+          url
+          parameters {
+            name
+            description
+            in
+            type
+            required
+            enum
+            allowNull
+            mapToData
+            validation
+            alias
+            deprecated
+          }
+          headers {
+            name
+            value
+          }
+          previews(required: true) {
+            name
+          }
+          renamed {
+            before(format: CAMELCASE)
+            after(format: CAMELCASE)
+            date
+            note
+          }
         }
       }
-      if (param.enum) {
-        result[param.name].enum = param.enum;
+    `,
+    { url: "https://octokit-routes-graphql-server.now.sh/" }
+  );
+
+  endpoints.forEach(endpoint => {
+    const scope = endpoint.scope;
+
+    if (!newRoutes[scope]) {
+      newRoutes[scope] = {};
+    }
+
+    const idName = endpoint.id;
+    const url = endpoint.url.toLowerCase().replace(/\{(\w+)\}/g, ":$1");
+
+    // new route
+    newRoutes[scope][idName] = {
+      method: endpoint.method,
+      headers: endpoint.headers.reduce((result, header) => {
+        if (!result) {
+          result = {};
+        }
+        result[header.name] = header.value;
+        return result;
+      }, undefined),
+      params: endpoint.parameters.reduce((result, param) => {
+        result[param.name] = {
+          type: param.type
+        };
+        if (param.allowNull) {
+          result[param.name].allowNull = true;
+        }
+        if (param.required) {
+          result[param.name].required = true;
+        }
+        if (param.mapToData) {
+          result[param.name].mapTo = "data";
+        }
+        if (param.enum) {
+          result[param.name].enum = param.enum;
+        }
+        if (param.validation) {
+          result[param.name].validation = param.validation;
+        }
+        if (param.deprecated) {
+          result[param.name].deprecated = true;
+
+          if (param.alias) {
+            result[param.name].alias = param.alias;
+            result[param.name].type = result[param.alias].type;
+          } else {
+            result[param.name].type = param.type;
+            result[param.name].description = param.description;
+          }
+        }
+
+        return result;
+      }, {}),
+      url
+    };
+
+    const previewHeaders = endpoint.previews
+      .map(preview => `application/vnd.github.${preview.name}-preview+json`)
+      .join(",");
+
+    if (previewHeaders) {
+      newRoutes[scope][idName].headers = {
+        accept: previewHeaders
+      };
+    }
+
+    if (endpoint.renamed) {
+      newRoutes[scope][
+        idName
+      ].deprecated = `octokit.${scope}.${endpoint.renamed.before}() has been renamed to octokit.${scope}.${endpoint.renamed.after}() (${endpoint.renamed.date})`;
+    }
+
+    if (endpoint.isDeprecated) {
+      newRoutes[scope][
+        idName
+      ].deprecated = `octokit.${scope}.${idName}() is deprecated, see ${endpoint.documentationUrl}`;
+    }
+  });
+
+  // temporary workaround to bring back obsolete `octokit.repos.getCommitRefSha()` method
+  newRoutes.repos.getCommitRefSha = {
+    deprecated:
+      '"Get the SHA-1 of a commit reference" will be removed. Use "Get a single commit" instead with media type format set to "sha" instead.',
+    method: "GET",
+    params: {
+      owner: {
+        required: true,
+        type: "string"
+      },
+      ref: {
+        required: true,
+        type: "string"
+      },
+      repo: {
+        required: true,
+        type: "string"
       }
-      if (param.regex) {
-        result[param.name].validation = param.regex;
-      }
-      if (param.deprecated) {
-        result[param.name].deprecated = true;
-        result[param.name].alias = param.deprecated.after.name;
-      }
-      return result;
-    }, {}),
-    url: endpoint.path
+    },
+    url: "/repos/:owner/:repo/commits/:ref"
   };
 
-  const previewHeaders = endpoint.previews
-    .filter(preview => preview.required)
-    .map(preview => `application/vnd.github.${preview.name}-preview+json`)
-    .join(",");
+  // Temporary workarounds to bring back endpoints that got lost due to transition to OpenAPI spec of octokit/routes
+  // New API endpoints that will replace these are in the works, so that workaround can be removed soon
+  newRoutes.git.listRefs = {
+    method: "GET",
+    params: {
+      namespace: {
+        type: "string"
+      },
+      owner: {
+        required: true,
+        type: "string"
+      },
+      page: {
+        type: "integer"
+      },
+      per_page: {
+        type: "integer"
+      },
+      repo: {
+        required: true,
+        type: "string"
+      }
+    },
+    url: "/repos/:owner/:repo/git/refs/:namespace"
+  };
+  newRoutes.issues.updateLabel = {
+    method: "PATCH",
+    params: {
+      color: {
+        type: "string"
+      },
+      current_name: {
+        required: true,
+        type: "string"
+      },
+      description: {
+        type: "string"
+      },
+      name: {
+        type: "string"
+      },
+      owner: {
+        required: true,
+        type: "string"
+      },
+      repo: {
+        required: true,
+        type: "string"
+      }
+    },
+    url: "/repos/:owner/:repo/labels/:current_name"
+  };
+  newRoutes.pulls.create = {
+    method: "POST",
+    params: {
+      base: {
+        required: true,
+        type: "string"
+      },
+      body: {
+        type: "string"
+      },
+      draft: {
+        type: "boolean"
+      },
+      head: {
+        required: true,
+        type: "string"
+      },
+      maintainer_can_modify: {
+        type: "boolean"
+      },
+      owner: {
+        required: true,
+        type: "string"
+      },
+      repo: {
+        required: true,
+        type: "string"
+      },
+      title: {
+        required: true,
+        type: "string"
+      }
+    },
+    url: "/repos/:owner/:repo/pulls"
+  };
 
-  if (previewHeaders) {
-    newRoutes[scope][idName].headers = {
-      accept: previewHeaders
-    };
-  }
+  // workaround to bring back the simpler `octokit.repos.uploadReleaseAsset()` method
+  newRoutes.repos.uploadReleaseAsset = {
+    method: "POST",
+    params: {
+      file: {
+        mapTo: "data",
+        required: true,
+        type: "string | object"
+      },
+      headers: {
+        required: true,
+        type: "object"
+      },
+      "headers.content-length": {
+        required: true,
+        type: "integer"
+      },
+      "headers.content-type": {
+        required: true,
+        type: "string"
+      },
+      label: {
+        type: "string"
+      },
+      name: {
+        required: true,
+        type: "string"
+      },
+      url: {
+        required: true,
+        type: "string"
+      }
+    },
+    url: ":url"
+  };
 
-  if (endpoint.deprecated) {
-    if (endpoint.deprecated.after) {
-      newRoutes[scope][idName].deprecated = `octokit.${scope}.${camelCase(
-        endpoint.deprecated.before.idName
-      )}() has been renamed to octokit.${scope}.${camelCase(
-        endpoint.deprecated.after.idName
-      )}() (${endpoint.deprecated.date})`;
-    } else {
-      newRoutes[scope][idName].deprecated = endpoint.deprecated.message;
-    }
-  }
-});
-
-writeFileSync(
-  ROUTES_PATH,
-  JSON.stringify(sortKeys(newRoutes, { deep: true }), null, 2) + "\n"
-);
-console.log(`${ROUTES_PATH} written.`);
+  writeFileSync(
+    ROUTES_PATH,
+    JSON.stringify(sortKeys(newRoutes, { deep: true }), null, 2) + "\n"
+  );
+  console.log(`${ROUTES_PATH} written.`);
+}
