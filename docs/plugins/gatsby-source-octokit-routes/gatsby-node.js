3ec96bfb5926b0b0488fb0bb3ec0c38793c57621
Gregor Martynus <gregor@martynus.net>
2019-09-17 16:10:04 -0700

    docs: @octokit/routes -> octokit-routes-graphql-server

--- docs/plugins/gatsby-source-octokit-routes/gatsby-node.js
+++ docs/plugins/gatsby-source-octokit-routes/gatsby-node.js
@@ -1,66 +1,119 @@
 const _ = require("lodash");
-const ROUTES = require("@octokit/routes");
+const { graphql } = require("@octokit/graphql");
 
 exports.sourceNodes = async ({ actions }) => {
   const { createNode } = actions;
 
-  Object.keys(ROUTES).map(scope => {
-    const scopeId = `octokit-routes-${scope}`;
-    const methods = [];
-
-    ROUTES[scope].forEach(endpoint => {
-      if (/legacy/.test(endpoint.idName)) {
-        return;
+  const { endpoints } = await graphql(
+    `
+      {
+        endpoints(filter: { isLegacy: false, isGithubCloudOnly: false }) {
+          isDeprecated
+          documentationUrl
+          scope(format: CAMELCASE)
+          id(format: CAMELCASE)
+          name
+          description
+          method
+          url
+          parameters {
+            name
+            description
+            in
+            type
+            required
+            enum
+            allowNull
+            mapToData
+            validation
+            alias
+            deprecated
+          }
+          headers {
+            name
+            value
+          }
+          previews(required: true) {
+            name
+          }
+          renamed {
+            before(format: CAMELCASE)
+            after(format: CAMELCASE)
+            date
+            note
+          }
+          responses {
+            code
+            description
+            examples {
+              data
+            }
+          }
+        }
       }
+    `,
+    { url: "https://octokit-routes-graphql-server.now.sh/" }
+  );
 
-      const endpointId = `octokit-routes-${scope}-${_.kebabCase(
-        endpoint.idName
-      )}`;
-      const paramNames = endpoint.params
-        .filter(param => param.required)
-        .filter(param => !/\./.test(param.name))
-        .map(param => param.name);
-      const paramsString = paramNames.length
-        ? `{
+  const methodsByScope = {};
+
+  endpoints.forEach(endpoint => {
+    const scopeId = `octokit-routes-${endpoint.scope}`;
+    if (!methodsByScope[endpoint.scope]) {
+      methodsByScope[endpoint.scope] = [];
+    }
+
+    const endpointId = `${scopeId}-${_.kebabCase(endpoint.id)}`;
+    const paramNames = endpoint.parameters
+      .filter(param => param.required)
+      .filter(param => !/\./.test(param.name))
+      .map(param => param.name);
+    const paramsString = paramNames.length
+      ? `{
   ${paramNames.join(",\n  ")}
 }`
-        : "";
-      const example = `octokit.${scope}.${_.camelCase(
-        endpoint.idName
-      )}(${paramsString})`;
-      const method = {
-        id: endpointId,
-        scope,
-        ...endpoint,
-        example
-      };
-      createNode({
-        id: endpointId,
-        parent: scopeId,
-        children: [],
-        scope,
-        ...endpoint,
-        example,
-        internal: {
-          description: `${endpoint.name} Method`,
-          contentDigest: endpointId,
-          type: "OctokitApiMethod"
-        }
-      });
-      methods.push(method);
-    });
+      : "";
+    const example = `octokit.${endpoint.scope}.${_.camelCase(
+      endpoint.id
+    )}(${paramsString})`;
+    const method = {
+      ...endpoint,
+      id: endpointId,
+      example
+    };
 
     createNode({
-      id: scopeId,
-      parent: null,
-      children: methods.map(method => method.id),
-      methods: methods,
-      name: scope,
+      ...endpoint,
+      parent: scopeId,
+      children: [],
+      id: endpointId,
+      example,
       internal: {
-        description: `${scope} Scope`,
-        contentDigest: scope,
-        type: "OctokitApiGroup"
+        description: `${endpoint.name} Method`,
+        contentDigest: endpointId,
+        type: "OctokitApiMethod"
       }
     });
+    methodsByScope[endpoint.scope].push(method);
   });
+
+  Object.keys(methodsByScope)
+    .sort()
+    .forEach(scope => {
+      const scopeId = `octokit-routes-${scope}`;
+      const methods = methodsByScope[scope];
+
+      createNode({
+        id: scopeId,
+        parent: null,
+        children: methods.map(method => method.id),
+        methods: methods,
+        name: scope,
+        internal: {
+          description: `${scope} Scope`,
+          contentDigest: scope,
+          type: "OctokitApiGroup"
+        }
+      });
+    });
 };
