2e122f3a80c2f865e5f2e354d9bb506469308d01
Ram Nad <ramnad1999@gmail.com>
2019-10-12 09:40:19 +0530

    fixed scenerio/error-test.js

--- test/issues/1497-include-error-message-on-validation-error-test.js
+++ test/issues/1497-include-error-message-on-validation-error-test.js
@@ -24,7 +24,7 @@ describe("https://github.com/octokit/rest.js/issues/1497", () => {
         restrictions: { users: [], teams: [] }
       })
       .reply(403, {
-        message: "Validation failed",
+        message: "Validation Failed",
         errors: [
           "Only organization repositories can have users and team restrictions"
         ],
@@ -58,7 +58,7 @@ describe("https://github.com/octokit/rest.js/issues/1497", () => {
       .catch(error => {
         expect(error).to.have.property(
           "message",
-          `Validation failed: "Only organization repositories can have users and team restrictions"`
+          `Validation Failed: "Only organization repositories can have users and team restrictions"`
         );
       });
   });
