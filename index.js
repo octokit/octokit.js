3933c247a40c1f2aaf988b4abb3ed8f0a601fa9f
Gregor Martynus <gregor@martynus.net>
2019-10-23 13:33:12 -0700

    fix: remove obsolete plugin to normalize git reference responses

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const CORE_PLUGINS = [
   require("./plugins/authentication-deprecated"), // deprecated: remove in v17
   require("./plugins/authentication"),
   require("./plugins/pagination"),
-  require("./plugins/normalize-git-reference-responses"),
   require("./plugins/register-endpoints"),
   require("./plugins/rest-api-endpoints"),
   require("./plugins/validate"),
