350932368a5ef77a8c1d8bd0a873ef4257e3ce90
Gregor Martynus <gregor@martynus.net>
2020-01-24 13:06:58 -0800

    build(Typescript): update types

--- index.d.ts
+++ index.d.ts
@@ -31948,6 +31948,11 @@ declare namespace Octokit {
 
     username: string;
   };
+  export type TeamsRemoveMembershipParams = {
+    team_id: number;
+
+    username: string;
+  };
   export type TeamsRemoveMembershipInOrgParams = {
     org: string;
 
@@ -40120,6 +40125,23 @@ declare class Octokit {
     removeMemberLegacy: {
       (
         params?: Octokit.RequestOptions & Octokit.TeamsRemoveMemberLegacyParams
+      ): Promise<Octokit.AnyResponse>;
+
+      endpoint: Octokit.Endpoint;
+    };
+    /**
+     * **Deprecation Notice:** This endpoint route is deprecated and will be removed from the Teams API. We recommend migrating your existing code to use the new [`Remove team membership`](https://developer.github.com/v3/teams/members/#remove-team-membership) endpoint.
+     *
+     * Team synchronization is available for organizations using GitHub Enterprise Cloud. For more information, see [GitHub's products](https://help.github.com/github/getting-started-with-github/githubs-products) in the GitHub Help documentation.
+     *
+     * To remove a membership between a user and a team, the authenticated user must have 'admin' permissions to the team or be an owner of the organization that the team is associated with. Removing team membership does not delete the user, it just removes their membership from the team.
+     *
+     * **Note:** When you have team synchronization set up for a team with your organization's identity provider (IdP), you will see an error if you attempt to use the API for making changes to the team's membership. If you have access to manage group membership in your IdP, you can manage GitHub team membership through your identity provider, which automatically adds and removes team members in an organization. For more information, see "[Synchronizing teams between your identity provider and GitHub](https://help.github.com/articles/synchronizing-teams-between-your-identity-provider-and-github/)."
+     * @deprecated octokit.teams.removeMembership() is deprecated, see https://developer.github.com/v3/teams/members/#remove-team-membership-legacy
+     */
+    removeMembership: {
+      (
+        params?: Octokit.RequestOptions & Octokit.TeamsRemoveMembershipParams
       ): Promise<Octokit.AnyResponse>;
 
       endpoint: Octokit.Endpoint;
