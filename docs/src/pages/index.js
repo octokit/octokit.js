b8a8893c26799e909de61cb40c374636dc730bb9
Trevor Blades <tdblades@gmail.com>
2020-02-16 12:11:09 -0800

    Use currentVersion config in html redirect

---
+++ docs/src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+
+export default function Index(props) {
+  const { currentVersion } = props.data.sitePlugin.pluginOptions;
+  return (
+    <Helmet>
+      <meta http-equiv="refresh" content={`0;url=/${currentVersion}`} />
+    </Helmet>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+    sitePlugin(name: { eq: "gatsby-plugin-versioned-docs" }) {
+      pluginOptions {
+        currentVersion
+      }
+    }
+  }
+`;
