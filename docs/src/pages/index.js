a8e2a882874e965892f959716dc1d1b587863dca
Trevor Blades <tdblades@gmail.com>
2020-02-18 15:58:40 -0800

    Add a couple extra comments

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -6,6 +6,7 @@ export default function Index(props) {
   const { currentVersion } = props.data.sitePlugin.pluginOptions;
   return (
     <Helmet>
+      {/* redirect the root path "/" to the current version */}
       <meta http-equiv="refresh" content={`0;url=/${currentVersion}`} />
     </Helmet>
   );
@@ -13,6 +14,7 @@ export default function Index(props) {
 
 export const pageQuery = graphql`
   {
+    # query for the configured current version slug
     sitePlugin(name: { eq: "gatsby-plugin-versioned-docs" }) {
       pluginOptions {
         currentVersion
