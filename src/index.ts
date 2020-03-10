68c715073d1203597130fd35768c6cc0ea895c68
Maxim Devoir <maximdevoir@gmail.com>
2020-03-09 21:23:23 -0700

    chore: describe type-widening issue.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 import { VERSION } from "./version";
 
 export const Octokit = Core.plugin([
+  // Workaround to prevent TypeScript from widening the inferred return type of
+  // plugins passed to Octokit, which would result in type information (e.g.
+  // methods provided by plugins) not being added to Octokit instances.
+  //
+  // See https://github.com/octokit/core.js/issues/51#issuecomment-596846088
   (requestLog as unknown) as () => void,
   restEndpointMethods,
   paginateRest
