1bdd1770ad6f91c3acd6287fab752567c159bdfb
Maxim Devoir <maximdevoir@gmail.com>
2020-03-09 22:04:34 -0700

    fix: load all plugins while preserving type information provided to Octokit (#1638)

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,15 @@ import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 
 import { VERSION } from "./version";
 
-export const Octokit = Core
-  // TODO: this really should be
+export const Octokit = Core.plugin([
+  // Workaround to prevent TypeScript from widening the inferred return type of
+  // plugins passed to Octokit, which would result in type information (e.g.
+  // methods provided by plugins) not being added to Octokit instances.
   //
-  //     .plugin([requestLog, paginateRest, restEndpointMethods])
-  //
-  // but for mystical reasons, using the line above does set the resulting the
-  // `octokit` instance type correctly. Neither `octokit.paginate()` nor all the
-  // endpoint methods such as `octokit.repos.get() are set
-  .plugin(requestLog)
-  .plugin([paginateRest, restEndpointMethods])
-  .defaults({
-    userAgent: `octokit-rest.js/${VERSION}`
-  });
+  // See https://github.com/octokit/core.js/issues/51#issuecomment-596846088
+  (requestLog as unknown) as () => void,
+  restEndpointMethods,
+  paginateRest
+]).defaults({
+  userAgent: `octokit-rest.js/${VERSION}`
+});
