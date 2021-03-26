7d85e1114f220efc11bc28efd6d188beb75af563
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-26 16:58:31 -0700

    refactor: adapt for `@octokit/plugin-rest-endpoint-methods` v5

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Octokit as Core } from "@octokit/core";
 import { requestLog } from "@octokit/plugin-request-log";
 import { paginateRest } from "@octokit/plugin-paginate-rest";
-import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+import { legacyRestEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 export { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
 
 import { VERSION } from "./version";
 
 export const Octokit = Core.plugin(
   requestLog,
-  restEndpointMethods,
+  legacyRestEndpointMethods,
   paginateRest
 ).defaults({
   userAgent: `octokit-rest.js/${VERSION}`,
