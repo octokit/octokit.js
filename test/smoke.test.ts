77a6c56f3ede0e467f875e1e46b0a3365dcb0794
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-29 18:00:15 -0700

    feat: `App` and `OAuthApp` clients

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,14 +1,56 @@
-import { Octokit } from "../src";
+import { Octokit, App, OAuthApp } from "../src";
 
 describe("Smoke tests", () => {
-  it("is a function", () => {
+  it("Octokit is a function", () => {
     expect(Octokit).toBeInstanceOf(Function);
     expect(() => new Octokit()).not.toThrow();
   });
 
-  it("can be used as a type", () => {
+  it("Octokit can be used as a type", () => {
     let octokit: Octokit;
     octokit = new Octokit();
     expect(octokit.rest.repos.get).toBeInstanceOf(Function);
+  });
+
+  it("App is a function", () => {
+    expect(App).toBeInstanceOf(Function);
+    expect(
+      () =>
+        new App({
+          appId: 123,
+          privateKey: "",
+        })
+    ).not.toThrow();
+  });
+
+  it("App can be used as a type", () => {
+    let app: App;
+    app = new App({
+      appId: 123,
+      privateKey: "",
+    });
+
+    expect(app.octokit.rest.repos.get).toBeInstanceOf(Function);
+  });
+
+  it("OAuthApp is a function", () => {
+    expect(OAuthApp).toBeInstanceOf(Function);
+    expect(
+      () =>
+        new OAuthApp({
+          clientId: "",
+          clientSecret: "",
+        })
+    ).not.toThrow();
+  });
+
+  it("OAuthApp can be used as a type", () => {
+    let app: OAuthApp;
+    app = new OAuthApp({
+      clientId: "",
+      clientSecret: "",
+    });
+
+    expect(app.octokit.rest.repos.get).toBeInstanceOf(Function);
   });
 });
