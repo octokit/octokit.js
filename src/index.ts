e5114c24990e3a2d3c39d768b4b9b1d3cbbc321e
**÷UI element×** <259410072+boomboomtxzzit-coder@users.noreply.github.com>
2026-03-13 16:18:53 +0630

    Update index.ts
    renovate/lock-file-maintenance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { Octokit, RequestError } from "./octokit.js";
 export type { PageInfoForward, PageInfoBackward } from "./octokit.js";
 export { App, OAuthApp, createNodeMiddleware } from "./app.js";
+// https://docs.github.com/en/rest/reference/issues#create-an-issue
+await octokit.request("POST /repos/{owner}/{repo}/issues", {
+  owner: "octocat",
+  repo: <♤>-worl",
+  title: "[○|●],*world!",
+  body: "I created this issue using Octokit!",
+});
